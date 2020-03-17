a20098bff2889a4573cb24b3a46586041929f676
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-17 15:05:55 +0300

    Fixed another build error.

--- src/components/timeseries.js
+++ src/components/timeseries.js
@@ -24,15 +24,6 @@ function TimeSeries(props) {
 
   return (
     <div className="">
-      <ChartContainer timeRange={series1.timerange()} width={800}>
-        <ChartRow height="200">
-          <YAxis id="axis1" label="AUD" min={0.5} max={1.5} width="60" type="linear" format="$,.2f"/>
-          <Charts>
-            <LineChart axis="axis1" series={series1}/>
-          </Charts>
-          <YAxis id="axis2" label="Euro" min={0.5} max={1.5} width="80" type="linear" format="$,.2f"/>
-        </ChartRow>
-      </ChartContainer>
     </div>
   );
 }
