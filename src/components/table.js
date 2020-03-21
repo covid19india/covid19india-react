9f84c123a1908087858c9bfd6af8b4cf1be0d7e7
Naveen S <coolnaveen1996@gmail.com>
2020-03-22 03:53:40 +0530

    Table Sort Changes

--- src/components/table.js
+++ src/components/table.js
@@ -4,20 +4,87 @@ import Row from './row';
 
 function Table(props) {
   const [states, setStates] = useState(props.states);
+  const [sortData, setSortData] = useState({
+    sortColumn: 'confirmed',
+    isAscending: false,
+  });
 
   useEffect(()=>{
     setStates(props.states);
   }, [props.states]);
 
+  const doSort = (e, props) => {
+
+    const totalRow = states.splice(0, 1);
+    console.log(totalRow)
+    states.sort((StateData1, StateData2) => {
+      const sortColumn = sortData.sortColumn;
+      let value1 = StateData1[sortColumn];
+      let value2 = StateData2[sortColumn];
+
+      if (sortColumn != 'state') {
+        value1 = parseInt(StateData1[sortColumn]);
+        value2 = parseInt(StateData2[sortColumn]);
+      }
+
+      if (sortData.isAscending) {
+        return value1 > value2? 1 : -1;
+      } else {
+        return value1 > value2? -1 : 1;
+      }
+    });
+    console.log(states);
+    states.unshift(totalRow[0]);
+  };
+
+  const handleSort = (e, props) => {
+    const currentsortColumn = e.currentTarget.querySelector('abbr').getAttribute('title').toLowerCase();
+    setSortData({
+      sortColumn: currentsortColumn,
+      isAscending: sortData.sortColumn == currentsortColumn? !sortData.isAscending : sortData.sortColumn === 'state',
+    });
+  };
+
+  doSort();
+
+  console.log(states);
+
   return (
     <table className="table fadeInUp" style={{animationDelay: '1s'}}>
       <thead>
         <tr>
-          <th className="state-heading">State/UT</th>
-          <th><abbr className={`${window.innerWidth <=769 ? 'is-cherry' : ''}`} title="Confirmed">{window.innerWidth <=769 ? 'Confirmed' : 'Confirmed'}</abbr></th>
-          <th><abbr className={`${window.innerWidth <=769 ? 'is-blue' : ''}`} title="Active">{window.innerWidth <=769 ? 'Active' : 'Active'}</abbr></th>
-          <th><abbr className={`${window.innerWidth <=769 ? 'is-green' : ''}`} title="Recovered">{window.innerWidth <=769 ? 'Recovered' : 'Recovered'}</abbr></th>
-          <th><abbr className={`${window.innerWidth <=769 ? 'is-gray' : ''}`} title="Deceased">{window.innerWidth <=769 ? 'Deaths' : 'Deaths'}</abbr></th>
+          <th className="state-heading" onClick={(e) => handleSort(e, props)} >
+            <div className='heading-content'>
+              <abbr title="State">
+                  State/UT
+              </abbr>
+              <div className={ sortData.sortColumn == 'state'? 'sort-black': ''}></div>
+            </div>
+          </th>
+          <th onClick={(e) => handleSort(e, props)}>
+            <div className='heading-content'>
+              <abbr className={`${window.innerWidth <=769 ? 'is-cherry' : ''}`} title="Confirmed">{window.innerWidth <=769 ? 'Confirmed' : 'Confirmed'}</abbr>
+              <div className={ sortData.sortColumn == 'confirmed'? 'sort-black' : '' }></div>
+            </div>
+          </th>
+          <th onClick={(e) => handleSort(e, props)}>
+            <div className='heading-content'>
+              <abbr className={`${window.innerWidth <=769 ? 'is-blue' : ''}`} title="Active">{window.innerWidth <=769 ? 'Active' : 'Active'}</abbr>
+              <div className={ sortData.sortColumn == 'active'? 'sort-black' : ''}></div>
+            </div>
+          </th>
+          <th onClick={(e) => handleSort(e, props)}>
+            <div className='heading-content'>
+              <abbr className={`${window.innerWidth <=769 ? 'is-green' : ''}`} title="Recovered">{window.innerWidth <=769 ? 'Recovered' : 'Recovered'}</abbr>
+              <div className={ sortData.sortColumn == 'recovered'? 'sort-black' : ''}></div>
+            </div>
+          </th>
+          <th onClick={(e) => handleSort(e, props)}>
+            <div className='heading-content'>
+              <abbr className={`${window.innerWidth <=769 ? 'is-gray' : ''}`} title="Deaths">{window.innerWidth <=769 ? 'Deaths' : 'Deaths'}</abbr>
+              <div className={ sortData.sortColumn == 'deaths'? 'sort-black' : ''}></div>
+            </div>
+          </th>
         </tr>
       </thead>
 
@@ -31,6 +98,7 @@ function Table(props) {
             }
           })
         }
+        { states.length > 1 && <Row key={0} state={states[0]}/> }
       </tbody>
 
     </table>
