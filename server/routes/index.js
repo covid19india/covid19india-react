8d1b5e46928bc35c7b17628d9468e36bd9e659e1
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-21 05:22:16 +0300

    Added meta tags.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const puppeteer = require('puppeteer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Express'});
+});
+
+router.get('/update_screenshot', (req, res, next) => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 600,
+      height: 600,
+      deviceScaleFactor: 1,
+      isMobile: true,
+    });
+    await page.goto('http://localhost:3001/summary', {'waitUntil': 'networkidle0'});
+    await page.screenshot({path: '../public/thumbnail.png'});
+
+    await browser.close();
+  })();
+  res.sendStatus(200);
 });
 
 module.exports = router;
