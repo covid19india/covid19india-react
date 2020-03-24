876b0e409fa570d9d6de8ae2dcc2255933c52ebb
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-24 14:56:25 +0300

    Added FAQs.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ function Navbar(props) {
 
   if (window.location.pathname!=='/summary') {
     return (
-      <div className="Navbar" style={{animationDelay: '0.5s'}}>
+      <div className="Navbar" style={{animationDelay: '0.5s', height: view==='Clusters' ? '2.5rem' : '', transition: 'all 0.3s ease-in-out'}}>
 
-        <img className="fadeInUp" src="/icon.png" style={{animationDelay: '0.0s'}}/>
+        <img className="fadeInUp" src="/icon.png" style={{animationDelay: '0.0s', width: view==='Clusters' ? '1.5rem' : '', height: view==='Clusters' ? '1.5rem' : '', transition: 'all 0.3s ease-in-out'}}/>
 
         <div className="navbar-left">
           <Link to="/" onClick={()=>{
@@ -23,16 +23,22 @@ function Navbar(props) {
             <span className={`fadeInUp ${view==='Updates' ? 'focused' : ''}`} style={{animationDelay: '0.2s'}}>Updates</span>
           </Link>*/}
 
-          <Link to="/networkmap" onClick={()=>{
-            setView('Cluster Map');
+          <Link to="/clusters" onClick={()=>{
+            setView('Clusters');
           }}>
-            <span className={`fadeInUp ${view==='Network Map' ? 'focused' : ''}`} style={{animationDelay: '0.3s'}}>Network Map</span>
+            <span className={`fadeInUp ${view==='Network Map' ? 'focused' : ''}`} style={{animationDelay: '0.3s'}}>Clusters</span>
           </Link>
 
           <Link to="/links" onClick={()=>{
             setView('Helpful Links');
           }}>
             <span className={`fadeInUp ${view==='Helpful Links' ? 'focused' : ''}`} style={{animationDelay: '0.4s'}}>Helpful Links</span>
+          </Link>
+
+          <Link to="/faqs" onClick={()=>{
+            setView('FAQs');
+          }}>
+            <span className={`fadeInUp ${view==='FAQs' ? 'focused' : ''}`} style={{animationDelay: '0.4s'}}>FAQs</span>
           </Link>
         </div>
 
