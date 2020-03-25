4a2a3af582754f9099234f7983fb839e4d66c17c
Abhishek Khandait <khandaitabhishek@gmail.com>
2020-03-25 20:35:36 +0530

    feat: darkmode

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar(props) {
   const [view, setView] = useState('Home');
+  const [theme, setTheme] = useState('theme-light');
 
-  if (window.location.pathname!=='/summary') {
+  useEffect(() => {
+    document.body.classList.remove(theme === 'theme-light' ? 'theme-dark' : 'theme-light');
+    document.body.classList.add(theme);
+  });
+
+  if (window.location.pathname !== '/summary') {
     return (
-      <div className="Navbar" style={{animationDelay: '0.5s', height: view==='Clusters' ? '2.5rem' : '', transition: 'all 0.3s ease-in-out'}}>
-
-        <img className="fadeInUp" src="/icon.png" style={{animationDelay: '0.0s', width: view==='Clusters' ? '1.5rem' : '', height: view==='Clusters' ? '1.5rem' : '', transition: 'all 0.3s ease-in-out'}}/>
+      <div
+        className="Navbar"
+        style={{
+          animationDelay: '0.5s',
+          height: view === 'Clusters' ? '2.5rem' : '',
+          transition: 'all 0.3s ease-in-out'
+        }}>
+        <img
+          className="fadeInUp"
+          src="/icon.png"
+          style={{
+            animationDelay: '0.0s',
+            width: view === 'Clusters' ? '1.5rem' : '',
+            height: view === 'Clusters' ? '1.5rem' : '',
+            transition: 'all 0.3s ease-in-out'
+          }}
+        />
 
         <div className="navbar-left">
-          <Link to="/" onClick={()=>{
-            setView('Home');
-          }}>
-            <span className={`fadeInUp ${view==='Home' ? 'focused' : ''}`} style={{animationDelay: '0.2s'}}>Home</span>
+          <Link
+            to="/"
+            onClick={() => {
+              setView('Home');
+            }}>
+            <span className={`fadeInUp ${view === 'Home' ? 'focused' : ''}`} style={{ animationDelay: '0.2s' }}>
+              Home
+            </span>
           </Link>
 
           {/* <Link to="/updates" onClick={()=>{
@@ -23,33 +47,56 @@ function Navbar(props) {
             <span className={`fadeInUp ${view==='Updates' ? 'focused' : ''}`} style={{animationDelay: '0.2s'}}>Updates</span>
           </Link>*/}
 
-          <Link to="/clusters" onClick={()=>{
-            setView('Clusters');
-          }}>
-            <span className={`fadeInUp ${view==='Network Map' ? 'focused' : ''}`} style={{animationDelay: '0.3s'}}>Clusters</span>
+          <Link
+            to="/clusters"
+            onClick={() => {
+              setView('Clusters');
+            }}>
+            <span className={`fadeInUp ${view === 'Clusters' ? 'focused' : ''}`} style={{ animationDelay: '0.3s' }}>
+              Clusters
+            </span>
           </Link>
 
-          <Link to="/links" onClick={()=>{
-            setView('Helpful Links');
-          }}>
-            <span className={`fadeInUp ${view==='Helpful Links' ? 'focused' : ''}`} style={{animationDelay: '0.4s'}}>Helpful Links</span>
+          <Link
+            to="/links"
+            onClick={() => {
+              setView('Helpful Links');
+            }}>
+            <span
+              className={`fadeInUp ${view === 'Helpful Links' ? 'focused' : ''}`}
+              style={{ animationDelay: '0.4s' }}>
+              Helpful Links
+            </span>
           </Link>
 
-          <Link to="/faqs" onClick={()=>{
-            setView('FAQs');
-          }}>
-            <span className={`fadeInUp ${view==='FAQs' ? 'focused' : ''}`} style={{animationDelay: '0.4s'}}>FAQs</span>
+          <Link
+            to="/faqs"
+            onClick={() => {
+              setView('FAQs');
+            }}>
+            <span className={`fadeInUp ${view === 'FAQs' ? 'focused' : ''}`} style={{ animationDelay: '0.4s' }}>
+              FAQs
+            </span>
           </Link>
         </div>
 
-        <div className="navbar-right"></div>
-
+        <div className="navbar-right fadeInUp">
+          <div className="toggle-box">
+            <input
+              type="checkbox"
+              name="checkbox1"
+              id="toggle-box-checkbox"
+              onClick={() => {
+                setTheme(theme === 'theme-dark' ? 'theme-light' : 'theme-dark');
+              }}
+            />
+            <label for="toggle-box-checkbox" className="toggle-box-label-left"></label>
+          </div>
+        </div>
       </div>
     );
   } else {
-    return (
-      <div></div>
-    );
+    return <div></div>;
   }
 }
 
