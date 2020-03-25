37fec27dbb98dea67ce27552cdd4576978bdeaee
shajahan <shah@metalimit.com>
2020-03-25 23:12:54 +0530

    Support State Deltas #12 implemented

--- src/components/row.js
+++ src/components/row.js
@@ -1,19 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Row(props) {
   const [state, setState] = useState(props.state);
 
-  useEffect(()=>{
+  useEffect(() => {
     setState(props.state);
   }, [props.state]);
 
   return (
     <tr className={props.total ? 'is-total' : ''}>
-      <td style={{fontWeight: 600}}>{state.state}</td>
-      <td>{state.confirmed}</td>
-      <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.active)===0 ? '-' : state.active}</td>
-      <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.recovered)===0 ? '-' : state.recovered}</td>
-      <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.deaths)===0 ? '-' : state.deaths}</td>
+      <td style={{ fontWeight: 600 }}>{state.state}</td>
+      <td>
+        <span style={{ float: 'left', textAlign: 'right', width: '50%' }}>{state.confirmed}</span>
+        <span style={{ float: 'right', color: '#ff073a99' }}>
+          {state.delta.confirmed > 0 ? '+' + state.delta.confirmed : ''}
+        </span>
+      </td>
+      <td style={{ color: parseInt(state.active) === 0 ? '#B5B5B5' : 'inherit' }}>
+        <span style={{ float: 'left', textAlign: 'right', width: '50%' }}>{parseInt(state.active) === 0 ? '-' : state.active}&nbsp;</span>
+        <span style={{ float: 'right', color: '#007bff99' }}>
+          {state.delta.active > 0 ? '+' + state.delta.active : ''}
+        </span>
+      </td>
+      <td style={{ color: parseInt(state.recovered) === 0 ? '#B5B5B5' : 'inherit' }}>
+        <span style={{ float: 'left', textAlign: 'right', width: '50%' }}>{parseInt(state.recovered) === 0 ? '-' : state.recovered}</span>
+        <span style={{ float: 'right', color: '#28a74599' }}>
+          {state.delta.recovered > 0 ? '+' + state.delta.recovered : ''}
+        </span>
+      </td>
+      <td style={{ color: parseInt(state.deaths) === 0 ? '#B5B5B5' : 'inherit' }}>
+        <span style={{ float: 'left', textAlign: 'right', width: '50%' }}>{parseInt(state.deaths) === 0 ? '-' : state.deaths}</span>
+        <span style={{ float: 'right', color: '#6c757d99' }}>
+          {state.delta.deaths > 0 ? '+' + state.delta.deaths : ''}
+        </span>
+      </td>
     </tr>
   );
 }
