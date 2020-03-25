ef540e71bb0194acb14f5a78757472dbc1660048
Yogeshvar <yogeshvar.senthilkumar@learnship.com>
2020-03-25 22:21:27 +0530

    :art: update eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,30 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
+  env: {
+    browser: true,
+    es6: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+  extends: ['plugin:react/recommended', 'google'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    "require-jsdoc" : 0
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-console': 'warn',
+    'no-eval': 'error',
+    'import/first': 'error',
+    'require-jsdoc': 0,
   },
-  'settings': {
-    'react': {
-      'version': 'detect',
-    }
-  }
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
