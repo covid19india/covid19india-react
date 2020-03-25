04cf52dd868bea8c3072f067b0f62cefff0803bb
Ritabrata <ritabrata@Ritabratas-MacBook-Pro.local>
2020-03-25 19:11:05 +0530

    [Rito] #12 Add support for state deltas on row.js

--- src/components/row.js
+++ src/components/row.js
@@ -10,10 +10,33 @@ function Row(props) {
   return (
     <tr className={props.total ? 'is-total' : ''}>
       <td style={{fontWeight: 600}}>{state.state}</td>
-      <td>{state.confirmed}</td>
-      <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.active)===0 ? '-' : state.active}</td>
-      <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.recovered)===0 ? '-' : state.recovered}</td>
-      <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.deaths)===0 ? '-' : state.deaths}</td>
+      <td>
+        <span className="left"> {state.confirmed} </span>
+        <span
+          className="is-cherry"> {state.delta ? state.delta.confirmed >= 0 ? '+' + state.delta.confirmed : state.delta.confirmed : ''}
+                </span>
+      </td>
+
+      <td style={{color: parseInt(state.active) === 0 ? '#B5B5B5' : 'inherit'}}>
+        <span className="left"> {parseInt(state.active) === 0 ? '-' : state.active} </span>
+        <span
+          className="is-blue"> {state.delta ? state.delta.active >= 0 ? '+' + state.delta.active : state.delta.active : ''}
+                </span>
+      </td>
+
+      <td style={{color: parseInt(state.recovered) === 0 ? '#B5B5B5' : 'inherit'}}>
+        <span className="left"> {parseInt(state.recovered) === 0 ? '-' : state.recovered} </span>
+        <span
+          className="is-green"> {state.delta ? state.delta.recovered >= 0 ? '+' + state.delta.recovered : state.delta.recovered : ''}
+                </span>
+      </td>
+
+      <td style={{color: parseInt(state.deaths) === 0 ? '#B5B5B5' : 'inherit'}}>
+        <span className="left"> {parseInt(state.deaths) === 0 ? '-' : state.deaths} </span>
+        <span
+          className="is-grey"> {state.delta ? state.delta.deaths >= 0 ? '+' + state.delta.deaths : state.delta.deaths : ''}
+                </span>
+      </td>
     </tr>
   );
 }
