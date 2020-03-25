a364da9a6174466ab71915535431fd440b395bee
sumit ghosh <sumitmckv14@gmail.com>
2020-03-25 16:02:06 +0530

    Added state wise deltas

--- src/components/row.js
+++ src/components/row.js
@@ -10,10 +10,38 @@ function Row(props) {
   return (
     <tr className={props.total ? 'is-total' : ''}>
       <td style={{fontWeight: 600}}>{state.state}</td>
-      <td>{state.confirmed}</td>
-      <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.active)===0 ? '-' : state.active}</td>
-      <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.recovered)===0 ? '-' : state.recovered}</td>
-      <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>{parseInt(state.deaths)===0 ? '-' : state.deaths}</td>
+      <td>
+          {state.confirmed}
+          {state.delta.confirmed > 0 &&
+              <span style={{color: '#ff073a', float: 'right'}}>
+                  +{state.delta.confirmed}
+              </span>
+          }
+      </td>
+      <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>
+          {parseInt(state.active)===0 ? '-' : state.active}
+          {state.delta.active > 0 &&
+            <span style={{color: '#007bff', float: 'right'}}>
+                  +{state.delta.active}
+            </span>
+          }
+      </td>
+      <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>
+          {parseInt(state.recovered)===0 ? '-' : state.recovered}
+          {state.delta.recovered > 0 &&
+              <span style={{color: '#28a745', float: 'right'}}>
+                      +{state.delta.recovered}
+              </span>
+          }
+      </td>
+      <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>
+          {parseInt(state.deaths)===0 ? '-' : state.deaths}
+          {state.delta.deaths > 0 &&
+              <span style={{color: '#6c757d', float: 'right'}}>
+                   +{state.delta.deaths}
+              </span>
+          }
+      </td>
     </tr>
   );
 }
