e1bf67e2106d6efcde059b5d1501a21a614d4186
programworld999 <programworld999@gmail.com>
2020-03-25 19:58:55 +0530

    pwa

---
+++ public/service-worker.js
@@ -0,0 +1,48 @@
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+    'offlinePage.html',
+];
+
+let CACHE_NAME = 'INDIA COVID-19 TRACKER';
+
+
+// Install a service worker
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                    return cache.match('offlinePage.html');
+                });
+        })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    let cacheWhitelist = ['webcredit'];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
