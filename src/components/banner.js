317e0dfc0b73781e1714cfeeb126ed4815fb1d34
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-25 14:50:45 +0300

    Rethought logic.

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,13 +1,35 @@
 import React, {useState, useEffect} from 'react';
-import Ticker, {NewsTicker} from 'nice-react-ticker';
+import differenceInMilliseconds from 'date-fns/differenceInMilliseconds';
 import axios from 'axios';
 
 function Banner(props) {
   const [snippets, setSnippets] = useState([]);
+  const [snippet, setSnippet] = useState();
+  const [start, setStart] = useState(props.start ? new Date(props.date) : localStorage.getItem('start')==='null' ? new Date() : new Date(localStorage.getItem('start')));
+  const [difference, setDifference] = useState(new Date(differenceInMilliseconds(new Date(), start)).toISOString().slice(11, 19));
 
   useEffect(()=>{
     getSnippets();
   }, [1]);
+
+  useEffect(()=>{
+    if (snippets.length>1) {
+      setSnippet(snippets[0]);
+    }
+  }, [snippets]);
+
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      setDifference(new Date(differenceInMilliseconds(new Date(), start)).toISOString().slice(11, 19));
+    }, 10000);
+    snippetChooser(0, snippets.length-1);
+    return () => clearInterval(interval);
+  }, [difference]);
+
+  const snippetChooser = (min, max) => {
+    const index = Math.random() * (max - min) + min;
+    setSnippet(snippets[Math.floor(index)]);
+  };
 
   const getSnippets = () => {
     axios.get('https://api.covid19india.org/website_data.json')
@@ -21,13 +43,9 @@ function Banner(props) {
 
   return (
     <div className="Banner fadeInUp" style={{animationDelay: '0.5s'}}>
-      <Ticker isNewsTicker={true}>
-        {snippets.map((snippet, index)=>{
-          return (
-            <NewsTicker key={index} id={index} title={snippet.banner} />
-          );
-        })}
-      </Ticker>
+      <div className="snippet fadeInUp" style={{animationDelay: '0.5s'}}>
+        {snippet ? snippet.banner : ''} &nbsp;
+      </div>
     </div>
   );
 }
