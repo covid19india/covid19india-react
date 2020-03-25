1dca24c55b00be5feab37fa7968d92098977950c
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-26 01:12:38 +0300

    Tweaked design for improved readability.

--- src/components/row.js
+++ src/components/row.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import * as Icon from 'react-feather';
 
 function Row(props) {
   const [state, setState] = useState(props.state);
@@ -11,36 +12,32 @@ function Row(props) {
     <tr className={props.total ? 'is-total' : ''}>
       <td style={{fontWeight: 600}}>{state.state}</td>
       <td>
-          {state.confirmed}
-          {state.delta && state.delta.confirmed && state.delta.confirmed > 0 ?
-              <span style={{color: '#ff073a', float: 'right'}}>
-                  +{state.delta.confirmed}
-              </span>: ''
-          }
+        {parseInt(state.confirmed)===0 ? '-' : state.active}
+        <span className="deltas" style={{color: '#ff073a'}}>
+          {!state.delta.confirmed==0 && <Icon.ArrowUp/>}
+          {state.delta.confirmed > 0 ? `${state.delta.confirmed}` : ''}
+        </span>
       </td>
       <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>
-          {parseInt(state.active)===0 ? '-' : state.active}
-          {state.delta && state.delta.active && state.delta.active > 0 ?
-            <span style={{color: '#007bff', float: 'right'}}>
-                  +{state.delta.active}
-            </span>: ''
-          }
+        {parseInt(state.active)===0 ? '-' : state.active}
+        <span className="deltas" style={{color: '#007bff'}}>
+          {!state.delta.active==0 && <Icon.ArrowUp/>}
+          {state.delta.active>0 ? `${state.delta.active}` : ''}
+        </span>
       </td>
       <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>
-          {parseInt(state.recovered)===0 ? '-' : state.recovered}
-          {state.delta && state.delta.recovered && state.delta.recovered > 0 ?
-              <span style={{color: '#28a745', float: 'right'}}>
-                      +{state.delta.recovered}
-              </span>: ''
-          }
+        {parseInt(state.recovered)===0 ? '-' : state.recovered}
+        <span className="deltas" style={{color: '#28a745'}}>
+          {!state.delta.recovered==0 && <Icon.ArrowUp/>}
+          {state.delta.recovered > 0 ? `${state.delta.recovered}` : ''}
+        </span>
       </td>
       <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>
-          {parseInt(state.deaths)===0 ? '-' : state.deaths}
-          {state.delta && state.delta.deaths && state.delta.deaths > 0 ?
-              <span style={{color: '#6c757d', float: 'right'}}>
-                   +{state.delta.deaths}
-              </span>: ''
-          }
+        {parseInt(state.deaths)===0 ? '-' : state.deaths}
+        <span className="deltas" style={{color: '#6c757d'}}>
+          {!state.delta.deaths==0 && <Icon.ArrowUp/>}
+          {state.delta.deaths>0 ? `${state.delta.deaths}` : ''}
+        </span>
       </td>
     </tr>
   );
--- src/components/table.js
+++ src/components/table.js
@@ -77,7 +77,7 @@ function Table(props) {
           </th>
           <th onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
-              <abbr className={`${window.innerWidth <=769 ? 'is-cherry' : ''}`} title="Confirmed">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'C' : 'Cnfrmd' : 'Confirmed'}</abbr>
+              <abbr className={`${window.innerWidth <=769 ? 'is-cherry' : ''}`} title="Confirmed">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'C' : 'Cnfmd' : 'Confirmed'}</abbr>
               <div style={{display: sortData.sortColumn === 'confirmed' ? 'initial': 'none'}}><Icon.Maximize2/></div>
             </div>
           </th>
@@ -96,7 +96,7 @@ function Table(props) {
           </th>
           <th onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
-              <abbr className={`${window.innerWidth <=769 ? 'is-gray' : ''}`} title="Deaths">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'D' : 'DCSD' : 'Deaths'}</abbr>
+              <abbr className={`${window.innerWidth <=769 ? 'is-gray' : ''}`} title="Deaths">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'D' : 'Dcsd' : 'Deaths'}</abbr>
               <div style={{display: sortData.sortColumn === 'deaths' ? 'initial': 'none'}}><Icon.Maximize2/></div>
             </div>
           </th>
