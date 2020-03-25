1a052bd8cafbf66cd8baa2469f943a090dae7338
sumit ghosh <sumitmckv14@gmail.com>
2020-03-25 16:13:18 +0530

    Null check

--- src/components/row.js
+++ src/components/row.js
@@ -12,7 +12,7 @@ function Row(props) {
       <td style={{fontWeight: 600}}>{state.state}</td>
       <td>
           {state.confirmed}
-          {state.delta.confirmed > 0 &&
+          {state.delta && state.delta.confirmed && state.delta.confirmed > 0 &&
               <span style={{color: '#ff073a', float: 'right'}}>
                   +{state.delta.confirmed}
               </span>
@@ -20,7 +20,7 @@ function Row(props) {
       </td>
       <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>
           {parseInt(state.active)===0 ? '-' : state.active}
-          {state.delta.active > 0 &&
+          {state.delta && state.delta.active && state.delta.active > 0 &&
             <span style={{color: '#007bff', float: 'right'}}>
                   +{state.delta.active}
             </span>
@@ -28,7 +28,7 @@ function Row(props) {
       </td>
       <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>
           {parseInt(state.recovered)===0 ? '-' : state.recovered}
-          {state.delta.recovered > 0 &&
+          {state.delta && state.delta.recovered && state.delta.recovered > 0 &&
               <span style={{color: '#28a745', float: 'right'}}>
                       +{state.delta.recovered}
               </span>
@@ -36,7 +36,7 @@ function Row(props) {
       </td>
       <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>
           {parseInt(state.deaths)===0 ? '-' : state.deaths}
-          {state.delta.deaths > 0 &&
+          {state.delta && state.delta.deaths && state.delta.deaths > 0 &&
               <span style={{color: '#6c757d', float: 'right'}}>
                    +{state.delta.deaths}
               </span>
