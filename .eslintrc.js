d2c2c71544777e9238c4d628296d19b02580a145
Abhishek K <abhishekakade@gmail.com>
2020-03-26 20:45:51 +0530

    fix: set up eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
+  env: {
+    browser: true,
+    es6: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+  extends: ["plugin:react/recommended", "google", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    "require-jsdoc" : 0
+  plugins: ["react", "prettier"],
+  rules: {
+    "require-jsdoc": 0,
+    "prettier/prettier": "error",
   },
-  'settings': {
-    'react': {
-      'version': 'detect',
-    }
-  }
-};
+}
