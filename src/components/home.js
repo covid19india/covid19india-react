9bb7d1d2dad1e62ffc66b4b130324220f30c9dcb
Rohit Sharma <rohit2sharma95@gmail.com>
2020-03-26 21:15:24 +0530

    :bug: Active delta at top summary does not match with the one in table total row.
    Fixed by showing the data from the statewise array
    (First entry in the statewise array holds the correct values)

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {format, zonedTimeToUtc} from 'date-fns-tz';
-import {formatDistance} from 'date-fns';
+import { format, zonedTimeToUtc } from 'date-fns-tz';
+import { formatDistance } from 'date-fns';
 
 import Table from './table';
 import Level from './level';
@@ -20,24 +20,24 @@ function Home(props) {
   const [timeseriesMode, setTimeseriesMode] = useState(true);
   const [stateHighlighted, setStateHighlighted] = useState(undefined);
 
-  useEffect(()=> {
-    if (fetched===false) {
+  useEffect(() => {
+    if (fetched === false) {
       getStates();
     }
   }, [fetched]);
 
   const getStates = () => {
     axios.get('https://api.covid19india.org/data.json')
-        .then((response)=>{
-          setStates(response.data.statewise);
-          setTimeseries(response.data.cases_time_series);
-          setLastUpdated(formatDate(response.data.statewise[0].lastupdatedtime));
-          setDeltas(response.data.key_values[0]);
-          setFetched(true);
-        })
-        .catch((err)=>{
-          console.log(err);
-        });
+      .then((response) => {
+        setStates(response.data.statewise);
+        setTimeseries(response.data.cases_time_series);
+        setLastUpdated(formatDate(response.data.statewise[0].lastupdatedtime));
+        setDeltas(response.data.key_values[0]);
+        setFetched(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const formatDate = (unformattedDate) => {
@@ -51,14 +51,14 @@ function Home(props) {
 
   const onHighlightState = (state, index) => {
     if (!state && !index) setStateHighlighted(null);
-    else setStateHighlighted({state, index});
+    else setStateHighlighted({ state, index });
   };
 
   return (
     <div className="Home">
       <div className="home-left">
 
-        <div className="header fadeInUp" style={{animationDelay: '0.5s'}}>
+        <div className="header fadeInUp" style={{ animationDelay: '0.5s' }}>
           <div className="header-mid">
             <div className="titles">
               <h1>India COVID-19 Tracker</h1>
@@ -66,13 +66,13 @@ function Home(props) {
             </div>
             <div className="last-update">
               <h6>Last Updated</h6>
-              <h3>{isNaN(Date.parse(lastUpdated)) ? '' : formatDistance(zonedTimeToUtc(new Date(lastUpdated), 'Asia/Calcutta'), zonedTimeToUtc(new Date()))+' Ago'}</h3>
+              <h3>{isNaN(Date.parse(lastUpdated)) ? '' : formatDistance(zonedTimeToUtc(new Date(lastUpdated), 'Asia/Calcutta'), zonedTimeToUtc(new Date())) + ' Ago'}</h3>
             </div>
           </div>
         </div>
 
-        <Level data={states} deltas={deltas}/>
-        <Minigraph timeseries={timeseries} animate={true}/>
+        <Level data={states[0] || {}} />
+        <Minigraph timeseries={timeseries} animate={true} />
 
         <Table states={states} summary={false} onHighlightState={onHighlightState} />
 
@@ -82,15 +82,15 @@ function Home(props) {
 
         <ChoroplethMap states={states} stateHighlighted={stateHighlighted} />
 
-        <div className="timeseries-header fadeInUp" style={{animationDelay: '1.5s'}}>
+        <div className="timeseries-header fadeInUp" style={{ animationDelay: '1.5s' }}>
           <h1>Spread Trends</h1>
           <div className="tabs">
-            <div className={`tab ${graphOption===1 ? 'focused' : ''}`} onClick={()=>{
+            <div className={`tab ${graphOption === 1 ? 'focused' : ''}`} onClick={() => {
               setGraphOption(1);
             }}>
               <h4>Cumulative</h4>
             </div>
-            <div className={`tab ${graphOption===2 ? 'focused' : ''}`} onClick={()=>{
+            <div className={`tab ${graphOption === 2 ? 'focused' : ''}`} onClick={() => {
               setGraphOption(2);
             }}>
               <h4>Daily</h4>
@@ -99,14 +99,14 @@ function Home(props) {
 
           <div className="timeseries-mode">
             <label htmlFor="timeseries-mode">Scale Uniformly</label>
-            <input type="checkbox" checked={timeseriesMode} onChange={(event)=>{
+            <input type="checkbox" checked={timeseriesMode} onChange={(event) => {
               setTimeseriesMode(!timeseriesMode);
-            }}/>
+            }} />
           </div>
 
         </div>
 
-        <TimeSeries timeseries={timeseries} type={graphOption} mode={timeseriesMode}/>
+        <TimeSeries timeseries={timeseries} type={graphOption} mode={timeseriesMode} />
 
       </div>
     </div>
--- src/components/level.js
+++ src/components/level.js
@@ -1,34 +1,21 @@
 import React, {useState, useEffect} from 'react';
 
-function Level(props) {
-  const [data, setData] = useState(props.data);
+function Level({data}) {
   const [confirmed, setConfirmed] = useState(0);
   const [active, setActive] = useState(0);
   const [recoveries, setRecoveries] = useState(0);
   const [deaths, setDeaths] = useState(0);
 
-  useEffect(()=>{
-    setData(props.data);
+  useEffect(() => {
     parseData();
+    console.log('sdsd:- ', data);
   });
 
   const parseData = () => {
-    let confirmed = 0;
-    let active = 0;
-    let recoveries = 0;
-    let deaths = 0;
-    data.map((state, index) => {
-      if (index !== 0) {
-        confirmed += parseInt(state.confirmed);
-        active += parseInt(state.active);
-        recoveries += parseInt(state.recovered);
-        deaths += parseInt(state.deaths);
-      }
-    });
-    setConfirmed(confirmed);
-    setActive(active);
-    setRecoveries(recoveries);
-    setDeaths(deaths);
+    setConfirmed(data.confirmed);
+    setActive(data.active);
+    setRecoveries(data.recovered);
+    setDeaths(data.deaths);
   };
 
   return (
@@ -36,25 +23,25 @@ function Level(props) {
 
       <div className="level-item is-cherry">
         <h5>Confirmed</h5>
-        <h4>[{props.deltas ? props.deltas.confirmeddelta>=0 ? '+'+props.deltas.confirmeddelta : props.deltas.confirmeddelta : ''}]</h4>
+        <h4>[{data.delta ? (data.delta.confirmed >= 0 ? '+' + data.delta.confirmed : data.delta.confirmed) : ''}]</h4>
         <h1>{confirmed} </h1>
       </div>
 
       <div className="level-item is-blue">
         <h5 className="heading">Active</h5>
-        <h4>[{props.deltas ? props.deltas.confirmeddelta-props.deltas.recovereddelta-props.deltas.deceaseddelta >=0 ? '+'+(props.deltas.confirmeddelta-props.deltas.recovereddelta-props.deltas.deceaseddelta).toString() : props.deltas.confirmeddelta-props.deltas.recovereddelta-props.deltas.deceaseddelta : ''}]</h4>
+        <h4>[{data.delta ? (data.delta.active >= 0 ? '+' + data.delta.active : data.delta.active) : ''}]</h4>
         <h1 className="title has-text-info">{active}</h1>
       </div>
 
       <div className="level-item is-green">
         <h5 className="heading">Recovered</h5>
-        <h4>[{props.deltas ? props.deltas.recovereddelta>=0 ? '+'+props.deltas.recovereddelta : props.deltas.recovereddelta : ''}]</h4>
+        <h4>[{data.delta ? (data.delta.recovered >= 0 ? '+' + data.delta.recovered : data.delta.recovered) : ''}]</h4>
         <h1 className="title has-text-success">{recoveries} </h1>
       </div>
 
       <div className="level-item is-gray">
         <h5 className="heading">Deceased</h5>
-        <h4>[{props.deltas ? props.deltas.deceaseddelta>=0 ? '+'+props.deltas.deceaseddelta : '+0' : ''}]</h4>
+        <h4>[{data.delta ? (data.delta.deaths >= 0 ? '+' + data.delta.deaths : '+0') : ''}]</h4>
         <h1 className="title has-text-grey">{deaths}</h1>
       </div>
 
