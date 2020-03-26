55bc41fc38df9579690b9dd50d657a5cc8457c70
Shubham Singhm <shubhsingh01@gmail.com>
2020-03-27 00:08:48 +0530

    Added arrow to indicate the current sort order

--- src/components/table.js
+++ src/components/table.js
@@ -108,32 +108,42 @@ function Table(props) {
               <abbr title="State">
                   State/UT
               </abbr>
-              <div style={{display: sortData.sortColumn === 'state' ? 'initial': 'none'}}><Icon.Maximize2/></div>
+              <div style={{display: sortData.sortColumn === 'state' ? 'initial': 'none'}}>
+                {sortData.isAscending ? <Icon.ArrowDown/> : <Icon.ArrowUp/>}
+              </div>
             </div>
           </th>
           <th className="sticky" onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
               <abbr className={`${window.innerWidth <=769 ? 'is-cherry' : ''}`} title="Confirmed">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'C' : 'Cnfmd' : 'Confirmed'}</abbr>
-              <div style={{display: sortData.sortColumn === 'confirmed' ? 'initial': 'none'}}><Icon.Maximize2/></div>
+              <div style={{display: sortData.sortColumn === 'confirmed' ? 'initial': 'none'}}>
+                {sortData.isAscending ? <Icon.ArrowDown/> : <Icon.ArrowUp/>}
+              </div>
             </div>
           </th>
           <th className="sticky" onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
               <abbr className={`${window.innerWidth <=769 ? 'is-blue' : ''}`} title="Active">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'A' : 'Actv' : 'Active'}</abbr>
-              <div style={{display: sortData.sortColumn === 'active' ? 'initial': 'none'}}><Icon.Maximize2/></div>
+              <div style={{display: sortData.sortColumn === 'active' ? 'initial': 'none'}}>
+                {sortData.isAscending ? <Icon.ArrowDown/> : <Icon.ArrowUp/>}
+              </div>
             </div>
           </th>
           <th  className="sticky" onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
               <abbr className={`${window.innerWidth <=769 ? 'is-green' : ''}`} title="Recovered">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'R' : 'Rcvrd' : 'Recovered'}</abbr>
               <div className={ sortData.sortColumn === 'recovered'? 'sort-black' : ''}></div>
-              <div style={{display: sortData.sortColumn === 'recovered' ? 'initial': 'none'}}><Icon.Maximize2/></div>
+              <div style={{display: sortData.sortColumn === 'recovered' ? 'initial': 'none'}}>
+                {sortData.isAscending ? <Icon.ArrowDown/> : <Icon.ArrowUp/>}
+              </div>
             </div>
           </th>
           <th className="sticky" onClick={(e) => handleSort(e, props)}>
             <div className='heading-content'>
               <abbr className={`${window.innerWidth <=769 ? 'is-gray' : ''}`} title="Deaths">{window.innerWidth <=769 ? window.innerWidth <=375 ? 'D' : 'Dcsd' : 'Deceased'}</abbr>
-              <div style={{display: sortData.sortColumn === 'deaths' ? 'initial': 'none'}}><Icon.Maximize2/></div>
+              <div style={{display: sortData.sortColumn === 'deaths' ? 'initial': 'none'}}>
+                {sortData.isAscending ? <Icon.ArrowDown/> : <Icon.ArrowUp/>}
+              </div>
             </div>
           </th>
         </tr>
