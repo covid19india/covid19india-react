d8a392b5282f10c13fcb9f6229a8a2d899b166e8
jeremyphilemon <jeremyphilemon@outlook.com>
2020-03-26 12:30:44 +0300

    Added support for districts in table.

---
+++ src/components/state.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react';
+import * as Icon from 'react-feather';
+
+function State(props) {
+  const [state, setState] = useState(props.state);
+
+  useEffect(()=>{
+    setState(props.state);
+  }, [props.state]);
+
+  return (
+  	<tr>
+      <td style={{fontWeight: 600}}>{state.state}</td>
+      <td>
+        {parseInt(state.confirmed)===0 ? '-' : state.active}
+        <span className="deltas" style={{color: '#ff073a'}}>
+          {!state.delta.confirmed==0 && <Icon.ArrowUp/>}
+          {state.delta.confirmed > 0 ? `${state.delta.confirmed}` : ''}
+        </span>
+      </td>
+      <td style={{color: parseInt(state.active)===0 ? '#B5B5B5' : 'inherit'}}>
+        {parseInt(state.active)===0 ? '-' : state.active}
+        <span className="deltas" style={{color: '#007bff'}}>
+          {!state.delta.active==0 && <Icon.ArrowUp/>}
+          {state.delta.active>0 ? `${state.delta.active}` : ''}
+        </span>
+      </td>
+      <td style={{color: parseInt(state.recovered)===0 ? '#B5B5B5' : 'inherit'}}>
+        {parseInt(state.recovered)===0 ? '-' : state.recovered}
+        <span className="deltas" style={{color: '#28a745'}}>
+          {!state.delta.recovered==0 && <Icon.ArrowUp/>}
+          {state.delta.recovered > 0 ? `${state.delta.recovered}` : ''}
+        </span>
+      </td>
+      <td style={{color: parseInt(state.deaths)===0 ? '#B5B5B5' : 'inherit'}}>
+        {parseInt(state.deaths)===0 ? '-' : state.deaths}
+        <span className="deltas" style={{color: '#6c757d'}}>
+          {!state.delta.deaths==0 && <Icon.ArrowUp/>}
+          {state.delta.deaths>0 ? `${state.delta.deaths}` : ''}
+        </span>
+      </td>
+    </tr>
+  );
+}
+
+export default State;
