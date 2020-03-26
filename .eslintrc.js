d1aed12369a6f4e91f4d3e46dc269684fc37b356
vinay thallam <vinay.rgit@gmail.com>
2020-03-26 18:03:44 +0530

    turn off eslint configs that might conflict with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['plugin:react/recommended', 'google'],
+  extends: ['plugin:react/recommended', 'google', 'eslint-config-prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
