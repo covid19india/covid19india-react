3407f6a2b143df0cc0ef744dd6201d12fdf96773
syntaxhacker <jrohit072@gmail.com>
2020-03-27 15:32:32 +0530

    make async requests , added key attribute to tbody

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,28 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import differenceInMilliseconds from 'date-fns/differenceInMilliseconds';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import differenceInMilliseconds from "date-fns/differenceInMilliseconds";
+import axios from "axios";
 
 function Banner(props) {
   const [snippets, setSnippets] = useState([]);
   const [snippet, setSnippet] = useState();
-  const [start, setStart] = useState(props.start ? new Date(props.date) : localStorage.getItem('start')==='null' ? new Date() : new Date(localStorage.getItem('start')));
-  const [difference, setDifference] = useState(new Date(differenceInMilliseconds(new Date(), start)).toISOString().slice(11, 19));
+  const [start, setStart] = useState(
+    props.start
+      ? new Date(props.date)
+      : localStorage.getItem("start") === "null"
+      ? new Date()
+      : new Date(localStorage.getItem("start"))
+  );
+  const [difference, setDifference] = useState(
+    new Date(differenceInMilliseconds(new Date(), start))
+      .toISOString()
+      .slice(11, 19)
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     getSnippets();
   }, [1]);
 
-  useEffect(()=>{
-    if (snippets.length>1) {
+  useEffect(() => {
+    if (snippets.length > 1) {
       setSnippet(snippets[0]);
     }
   }, [snippets]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const interval = setInterval(() => {
-      setDifference(new Date(differenceInMilliseconds(new Date(), start)).toISOString().slice(11, 19));
+      setDifference(
+        new Date(differenceInMilliseconds(new Date(), start))
+          .toISOString()
+          .slice(11, 19)
+      );
     }, 10000);
-    snippetChooser(0, snippets.length-1);
+    snippetChooser(0, snippets.length - 1);
     return () => clearInterval(interval);
   }, [difference]);
 
@@ -31,21 +45,20 @@ function Banner(props) {
     setSnippet(snippets[Math.floor(index)]);
   };
 
-  const getSnippets = () => {
-    axios.get('https://api.covid19india.org/website_data.json')
-        .then((response)=>{
-          setSnippets(response.data.factoids);
-        })
-        .catch((err)=>{
-          console.log(err);
-        });
+  const getSnippets = async () => {
+    await axios
+      .get("https://api.covid19india.org/website_data.json")
+      .then(response => {
+        setSnippets(response.data.factoids);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
-    <div className="Banner fadeInUp" style={{animationDelay: '0.2s'}}>
-      <div className="snippet">
-        {snippet ? snippet.banner : ''} &nbsp;
-      </div>
+    <div className="Banner fadeInUp" style={{ animationDelay: "0.2s" }}>
+      <div className="snippet">{snippet ? snippet.banner : ""} &nbsp;</div>
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,46 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {format, zonedTimeToUtc} from 'date-fns-tz';
-import {formatDistance} from 'date-fns';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { format, zonedTimeToUtc } from "date-fns-tz";
+import { formatDistance } from "date-fns";
 
-import Table from './table';
-import Level from './level';
-import ChoroplethMap from './choropleth';
-import TimeSeries from './timeseries';
-import Minigraph from './minigraph';
-import Banner from './banner';
+import Table from "./table";
+import Level from "./level";
+import ChoroplethMap from "./choropleth";
+import TimeSeries from "./timeseries";
+import Minigraph from "./minigraph";
+import Banner from "./banner";
 
 function Home(props) {
   const [states, setStates] = useState([]);
   const [fetched, setFetched] = useState(false);
   const [graphOption, setGraphOption] = useState(1);
-  const [lastUpdated, setLastUpdated] = useState('');
+  const [lastUpdated, setLastUpdated] = useState("");
   const [timeseries, setTimeseries] = useState([]);
   const [deltas, setDeltas] = useState([]);
   const [timeseriesMode, setTimeseriesMode] = useState(true);
   const [stateHighlighted, setStateHighlighted] = useState(undefined);
 
-  useEffect(()=> {
-    if (fetched===false) {
+  useEffect(() => {
+    if (fetched === false) {
       getStates();
     }
   }, [fetched]);
 
-  const getStates = () => {
-    axios.get('https://api.covid19india.org/data.json')
-        .then((response)=>{
-          setStates(response.data.statewise);
-          setTimeseries(response.data.cases_time_series);
-          setLastUpdated(formatDate(response.data.statewise[0].lastupdatedtime));
-          setDeltas(response.data.key_values[0]);
-          setFetched(true);
-        })
-        .catch((err)=>{
-          console.log(err);
-        });
+  const getStates = async () => {
+    await axios
+      .get("https://api.covid19india.org/data.json")
+      .then(response => {
+        setStates(response.data.statewise);
+        setTimeseries(response.data.cases_time_series);
+        setLastUpdated(formatDate(response.data.statewise[0].lastupdatedtime));
+        setDeltas(response.data.key_values[0]);
+        setFetched(true);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
-  const formatDate = (unformattedDate) => {
+  const formatDate = unformattedDate => {
     const day = unformattedDate.slice(0, 2);
     const month = unformattedDate.slice(3, 5);
     const year = unformattedDate.slice(6, 10);
@@ -51,14 +52,13 @@ function Home(props) {
 
   const onHighlightState = (state, index) => {
     if (!state && !index) setStateHighlighted(null);
-    else setStateHighlighted({state, index});
+    else setStateHighlighted({ state, index });
   };
 
   return (
     <div className="Home">
       <div className="home-left">
-
-        <div className="header fadeInUp" style={{animationDelay: '0.5s'}}>
+        <div className="header fadeInUp" style={{ animationDelay: "0.5s" }}>
           <div className="header-mid">
             <div className="titles">
               <h1>India COVID-19 Tracker</h1>
@@ -66,48 +66,72 @@ function Home(props) {
             </div>
             <div className="last-update">
               <h6>Last Updated</h6>
-              <h3>{isNaN(Date.parse(lastUpdated)) ? '' : formatDistance(zonedTimeToUtc(new Date(lastUpdated), 'Asia/Calcutta'), zonedTimeToUtc(new Date()))+' Ago'}</h3>
+              <h3>
+                {isNaN(Date.parse(lastUpdated))
+                  ? ""
+                  : formatDistance(
+                      zonedTimeToUtc(new Date(lastUpdated), "Asia/Calcutta"),
+                      zonedTimeToUtc(new Date())
+                    ) + " Ago"}
+              </h3>
             </div>
           </div>
         </div>
 
-        <Level data={states} deltas={deltas}/>
-        <Minigraph timeseries={timeseries} animate={true}/>
+        <Level data={states} deltas={deltas} />
+        <Minigraph timeseries={timeseries} animate={true} />
 
-        <Table states={states} summary={false} onHighlightState={onHighlightState} />
-
+        <Table
+          states={states}
+          summary={false}
+          onHighlightState={onHighlightState}
+        />
       </div>
 
       <div className="home-right">
-
         <ChoroplethMap states={states} stateHighlighted={stateHighlighted} />
 
-        <div className="timeseries-header fadeInUp" style={{animationDelay: '1.5s'}}>
+        <div
+          className="timeseries-header fadeInUp"
+          style={{ animationDelay: "1.5s" }}
+        >
           <h1>Spread Trends</h1>
           <div className="tabs">
-            <div className={`tab ${graphOption===1 ? 'focused' : ''}`} onClick={()=>{
-              setGraphOption(1);
-            }}>
+            <div
+              className={`tab ${graphOption === 1 ? "focused" : ""}`}
+              onClick={() => {
+                setGraphOption(1);
+              }}
+            >
               <h4>Cumulative</h4>
             </div>
-            <div className={`tab ${graphOption===2 ? 'focused' : ''}`} onClick={()=>{
-              setGraphOption(2);
-            }}>
+            <div
+              className={`tab ${graphOption === 2 ? "focused" : ""}`}
+              onClick={() => {
+                setGraphOption(2);
+              }}
+            >
               <h4>Daily</h4>
             </div>
           </div>
 
           <div className="timeseries-mode">
             <label htmlFor="timeseries-mode">Scale Uniformly</label>
-            <input type="checkbox" checked={timeseriesMode} onChange={(event)=>{
-              setTimeseriesMode(!timeseriesMode);
-            }}/>
+            <input
+              type="checkbox"
+              checked={timeseriesMode}
+              onChange={event => {
+                setTimeseriesMode(!timeseriesMode);
+              }}
+            />
           </div>
-
         </div>
 
-        <TimeSeries timeseries={timeseries} type={graphOption} mode={timeseriesMode}/>
-
+        <TimeSeries
+          timeseries={timeseries}
+          type={graphOption}
+          mode={timeseriesMode}
+        />
       </div>
     </div>
   );
