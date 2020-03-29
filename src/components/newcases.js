56f568305b2c80f4a2fb624afa7df5fda42c8624
avnish-choudhary <acp.avnish@gmail.com>
2020-03-30 02:56:17 +0530

    code refactor by changing variable names

--- src/components/newcases.js
+++ src/components/newcases.js
@@ -1,28 +1,36 @@
 import React, {useState, useEffect} from 'react';
 
 function RateOfNewCases(props) {
-  const [yesterdayCases, setYesterdayCases] = useState(0);
-  const [todayCases, setTodayCases] = useState(0);
-  const [secondLastDayCases, setSecondLastDayCases] = useState(0);
-  const [secondLastDayDate, setSecondLastDayDate] = useState(0);
-  const [thirdLastDayCases, setThirdLastDayCases] = useState(0);
-  const [fourthLastDayCases, setFourthLastDayCases] = useState(0);
-  const [thirdLastDayDate, setThirdLastDayDate] = useState(0);
+  const [firstDayCases, setFirstDayCases] = useState(0);
+  const [firstDayDate, setFirstDayDate] = useState(0);
+  const [secondDayCases, setSecondDayCases] = useState(0);
+  const [secondDayDate, setSecondDayDate] = useState(0);
+  const [thirdDayCases, setThirdDayCases] = useState(0);
+  const [thirdDayDate, setThirdDayDate] = useState(0);
+  const [fourthDayCases, setFourthDayCases] = useState(0);
+  const [fourthDayDate, setFourthDayDate] = useState(0);
+  const [fifthDayCases, setFifthDayCases] = useState(0);
+  const [upArrow, setUpArrow] = useState(0);
+  const [downArrow, setDownArrow] = useState(0);
 
   useEffect(() => {
     parseData();
+    setUpArrow(String.fromCharCode(8593));
+    setDownArrow(String.fromCharCode(8595));
   });
 
   const parseData = () => {
     const length = props.timeseries.length;
     if (length > 0) {
-      setYesterdayCases(props.timeseries[length - 1]['dailyconfirmed']);
-      setSecondLastDayCases(props.timeseries[length - 2]['dailyconfirmed']);
-      setSecondLastDayDate(props.timeseries[length - 2]['date']);
-      setThirdLastDayCases(props.timeseries[length - 3]['dailyconfirmed']);
-      setThirdLastDayDate(props.timeseries[length - 3]['date']);
-      setFourthLastDayCases(props.timeseries[length - 4]['dailyconfirmed']);
-      setTodayCases(props.deltas.confirmeddelta);
+      setFirstDayCases(props.deltas.confirmeddelta);
+      setFirstDayDate('Today');
+      setSecondDayCases(props.timeseries[length - 1]['dailyconfirmed']);
+      setSecondDayDate('Yesterday');
+      setThirdDayCases(props.timeseries[length - 2]['dailyconfirmed']);
+      setThirdDayDate(props.timeseries[length - 2]['date']);
+      setFourthDayCases(props.timeseries[length - 3]['dailyconfirmed']);
+      setFourthDayDate(props.timeseries[length - 3]['date']);
+      setFifthDayCases(props.timeseries[length - 4]['dailyconfirmed']);
     }
   };
 
@@ -34,81 +42,69 @@ function RateOfNewCases(props) {
       <div className="Level fadeInUp" style={{animationDelay: '0.8s'}}>
         <div
           className={`level-item ${
-            thirdLastDayCases - fourthLastDayCases < 0
-              ? 'is-green'
-              : 'is-cherry'
+            fourthDayCases - fifthDayCases < 0 ? 'is-green' : 'is-cherry'
           }`}
         >
-          <h5 className="heading">{thirdLastDayDate}</h5>
-          <h2 className="title has-text-grey">{thirdLastDayCases}</h2>
+          <h5 className="heading">{fourthDayDate}</h5>
+          <h2 className="title has-text-grey">{fourthDayCases}</h2>
         </div>
 
         <div
           className={`level-item ${
-            secondLastDayCases - thirdLastDayCases < 0
-              ? 'is-green'
-              : 'is-cherry'
+            thirdDayCases - fourthDayCases < 0 ? 'is-green' : 'is-cherry'
           }`}
         >
           <h5 className="heading"> </h5>
           <h1>
-            {secondLastDayCases - thirdLastDayCases > 0
-              ? String.fromCharCode(8593)
-              : String.fromCharCode(8595)}
-            {Math.abs(secondLastDayCases - thirdLastDayCases)}
+            {thirdDayCases - fourthDayCases > 0 ? upArrow : downArrow}
+            {Math.abs(thirdDayCases - fourthDayCases)}
           </h1>
           <h1 className="title has-text-grey">{}</h1>
         </div>
 
         <div
           className={`level-item ${
-            secondLastDayCases - thirdLastDayCases < 0
-              ? 'is-green'
-              : 'is-cherry'
+            thirdDayCases - fourthDayCases < 0 ? 'is-green' : 'is-cherry'
           }`}
         >
-          <h5 className="heading">{secondLastDayDate}</h5>
-          <h2 className="title has-text-success">{secondLastDayCases} </h2>
+          <h5 className="heading">{thirdDayDate}</h5>
+          <h2 className="title has-text-success">{thirdDayCases} </h2>
         </div>
 
         <div
           className={`level-item ${
-            yesterdayCases - secondLastDayCases < 0 ? 'is-green' : 'is-cherry'
+            secondDayCases - thirdDayCases < 0 ? 'is-green' : 'is-cherry'
           }`}
         >
           <h5 className="heading has-text-info">{} </h5>
           <h1 className="has-text-info">
-            {yesterdayCases - secondLastDayCases > 0
-              ? String.fromCharCode(8593)
-              : String.fromCharCode(8595)}
-            {Math.abs(yesterdayCases - secondLastDayCases)}
+            {secondDayCases - thirdDayCases > 0 ? upArrow : downArrow}
+            {Math.abs(secondDayCases - thirdDayCases)}
           </h1>
           <h1 className="title has-text-grey">{}</h1>
         </div>
 
         <div
           className={`level-item ${
-            yesterdayCases - secondLastDayCases < 0 ? 'is-green' : 'is-cherry'
+            secondDayCases - thirdDayCases < 0 ? 'is-green' : 'is-cherry'
           }`}
         >
-          <h5 className="heading">Yesterday</h5>
-          <h2 className="title has-text-info">{yesterdayCases}</h2>
+          <h5 className="heading">{secondDayDate}</h5>
+          <h2 className="title has-text-info">{secondDayCases}</h2>
         </div>
 
         <div className="level-item is-blue">
           <h5 className="heading"> </h5>
           <h1>
-            {todayCases - yesterdayCases > 0
-              ? String.fromCharCode(8593)
-              : String.fromCharCode(8595)}
-            {Math.abs(todayCases - yesterdayCases)}*
+            {firstDayCases - secondDayCases > 0 ? upArrow : downArrow}
+            {Math.abs(firstDayCases - secondDayCases)}*
           </h1>
           <h1 className="title has-text-grey">{}</h1>
         </div>
 
         <div className="level-item is-blue">
-          <h5>Today</h5>
-          <h2> {todayCases}* </h2>
+          <h5>{firstDayDate}</h5>
+          <h2> {firstDayCases}* </h2>
         </div>
       </div>
     </div>
