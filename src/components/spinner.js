b449ac73a80eb002f7181e022c84932148476b7d
Vamshi krishna <vamshi9666@riseup.net>
2020-03-31 00:36:02 +0530

    loading make to screenwidth

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,5 +1,17 @@
 import React from 'react';
 // TODO spinner needs to updated here
-const Spinner = ({}) => <div>loading...</div>;
+const Spinner = ({}) => (
+  <div
+    style={{
+      height: 300,
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    loading...
+  </div>
+);
 
 export default Spinner;
