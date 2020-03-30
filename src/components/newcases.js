068a5acf3f36fd0cf860c9f50fd6ffddacf3633c
avnish-choudhary <acp.avnish@gmail.com>
2020-03-30 22:54:54 +0530

    code refactor and created new method now

--- src/components/newcases.js
+++ src/components/newcases.js
@@ -41,42 +41,34 @@ function RateOfNewCases(props) {
   function displayRateOfNewCases() {
     const rows = [];
     for (let index = casesMap.length - 1; index > 0; index--) {
+      const casesDiff = casesMap[index - 1].cases - casesMap[index].cases;
+      const isToday = casesMap[index - 1].date === 'Today';
       rows.push(
-        <React.Fragment>
+        <React.Fragment key={index}>
           {index < casesMap.length - 1 && (
             <div
               className={`level-item ${
-                casesMap[index - 1].cases - casesMap[index].cases < 0
-                  ? casesMap[index - 1].date == 'Today'
-                    ? 'is-blue'
-                    : 'is-green'
-                  : 'is-cherry'
+                casesDiff < 0 ? (isToday ? 'is-blue' : 'is-green') : 'is-cherry'
               }`}
             >
               <h5 className="heading"> </h5>
               <h1>
-                {casesMap[index - 1].cases - casesMap[index].cases > 0
-                  ? upArrow
-                  : downArrow}
-                {Math.abs(casesMap[index - 1].cases - casesMap[index].cases)}
-                {casesMap[index - 1].date == 'Today' ? '*' : ''}
+                {casesDiff > 0 ? upArrow : downArrow}
+                {Math.abs(casesDiff)}
+                {isToday ? '*' : ''}
               </h1>
-              <h1 className="title has-text-grey">{}</h1>
+              <h1 className="title has-text-grey"></h1>
             </div>
           )}
           <div
             className={`level-item ${
-              casesMap[index - 1].cases - casesMap[index].cases < 0
-                ? casesMap[index - 1].date == 'Today'
-                  ? 'is-blue'
-                  : 'is-green'
-                : 'is-cherry'
+              casesDiff < 0 ? (isToday ? 'is-blue' : 'is-green') : 'is-cherry'
             }`}
           >
             <h5 className="heading">{casesMap[index - 1].date}</h5>
             <h2 className="title has-text-grey">
               {casesMap[index - 1].cases}
-              {casesMap[index - 1].date == 'Today' ? '*' : ''}
+              {isToday ? '*' : ''}
             </h2>
           </div>
         </React.Fragment>
