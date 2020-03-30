688fcc7888b1cfcfdefe513c3c4cae69145d8388
avnish-choudhary <acp.avnish@gmail.com>
2020-03-30 21:20:45 +0530

    code refactor and created new method now

--- src/components/newcases.js
+++ src/components/newcases.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 
 function RateOfNewCases(props) {
   const [casesMap, setCasesMap] = useState(0);
-  const [length, setLength] = useState(0);
   const upArrow = String.fromCharCode(8593);
   const downArrow = String.fromCharCode(8595);
 
@@ -36,69 +35,66 @@ function RateOfNewCases(props) {
         },
       ];
       setCasesMap(casesDetails);
-      setLength(length);
     }
   };
 
-  return (
-    <div className="home-left">
-      {length > 0 && (
+  function displayRateOfNewCases() {
+    const rows = [];
+    for (let index = casesMap.length - 1; index > 0; index--) {
+      rows.push(
         <React.Fragment>
-          <h2 className="Level fadeInUp" style={{animationDelay: '0.8s'}}>
-            Rate of New Cases
-          </h2>
-          <div className="Level fadeInUp" style={{animationDelay: '0.8s'}}>
-            {(function () {
-              const rows = [];
-              for (let index = casesMap.length - 1; index > 0; index--) {
-                rows.push(
-                  <React.Fragment>
-                    {index < casesMap.length - 1 && (
-                      <div
-                        className={`level-item ${
-                          casesMap[index - 1].cases - casesMap[index].cases < 0
-                            ? casesMap[index - 1].date == 'Today'
-                              ? 'is-blue'
-                              : 'is-green'
-                            : 'is-cherry'
-                        }`}
-                      >
-                        <h5 className="heading"> </h5>
-                        <h1>
-                          {casesMap[index - 1].cases - casesMap[index].cases > 0
-                            ? upArrow
-                            : downArrow}
-                          {Math.abs(
-                            casesMap[index - 1].cases - casesMap[index].cases
-                          )}
-                          {casesMap[index - 1].date == 'Today' ? '*' : ''}
-                        </h1>
-                        <h1 className="title has-text-grey">{}</h1>
-                      </div>
-                    )}
-                    <div
-                      className={`level-item ${
-                        casesMap[index - 1].cases - casesMap[index].cases < 0
-                          ? casesMap[index - 1].date == 'Today'
-                            ? 'is-blue'
-                            : 'is-green'
-                          : 'is-cherry'
-                      }`}
-                    >
-                      <h5 className="heading">{casesMap[index - 1].date}</h5>
-                      <h2 className="title has-text-grey">
-                        {casesMap[index - 1].cases}
-                        {casesMap[index - 1].date == 'Today' ? '*' : ''}
-                      </h2>
-                    </div>
-                  </React.Fragment>
-                );
-              }
-              return rows;
-            })()}
+          {index < casesMap.length - 1 && (
+            <div
+              className={`level-item ${
+                casesMap[index - 1].cases - casesMap[index].cases < 0
+                  ? casesMap[index - 1].date == 'Today'
+                    ? 'is-blue'
+                    : 'is-green'
+                  : 'is-cherry'
+              }`}
+            >
+              <h5 className="heading"> </h5>
+              <h1>
+                {casesMap[index - 1].cases - casesMap[index].cases > 0
+                  ? upArrow
+                  : downArrow}
+                {Math.abs(casesMap[index - 1].cases - casesMap[index].cases)}
+                {casesMap[index - 1].date == 'Today' ? '*' : ''}
+              </h1>
+              <h1 className="title has-text-grey">{}</h1>
+            </div>
+          )}
+          <div
+            className={`level-item ${
+              casesMap[index - 1].cases - casesMap[index].cases < 0
+                ? casesMap[index - 1].date == 'Today'
+                  ? 'is-blue'
+                  : 'is-green'
+                : 'is-cherry'
+            }`}
+          >
+            <h5 className="heading">{casesMap[index - 1].date}</h5>
+            <h2 className="title has-text-grey">
+              {casesMap[index - 1].cases}
+              {casesMap[index - 1].date == 'Today' ? '*' : ''}
+            </h2>
           </div>
         </React.Fragment>
-      )}
+      );
+    }
+    return rows;
+  }
+
+  return (
+    <div className="home-left">
+      <React.Fragment>
+        <h2 className="Level fadeInUp" style={{animationDelay: '0.8s'}}>
+          Rate of New Cases
+        </h2>
+        <div className="Level fadeInUp" style={{animationDelay: '0.8s'}}>
+          {displayRateOfNewCases()}
+        </div>
+      </React.Fragment>
     </div>
   );
 }
