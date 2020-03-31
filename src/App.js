dc0457bf47d413123f2416ddd5eef33c8cc88bd1
Manoj Soni <er.manojsoni@hotmail.com>
2020-03-31 19:48:08 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -17,6 +17,26 @@ import Banner from './components/banner';
 
 const history = require('history').createBrowserHistory;
 
+// dark-mode added by @devmanojsoni
+const options = {
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode();
+darkmode.toggle();
+console.log(darkmode.isActivated());  // must return true
+// dark-mode code ends
+
 function App() {
   // Add a new page simply by adding a new entry in this array.
   const pages = [
