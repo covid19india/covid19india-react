b66767d980bc72ab0cdbf1768476c2822ab6d84e
jeremyphilemon <jeremyphilemon@outlook.com>
2020-04-01 11:48:04 +0300

    Removed seconds from absolute time.

--- src/utils/common-functions.js
+++ src/utils/common-functions.js
@@ -24,5 +24,5 @@ export const formatDateAbsolute = (unformattedDate) => {
   const day = unformattedDate.slice(0, 2);
   const month = unformattedDate.slice(3, 5);
   const time = unformattedDate.slice(11);
-  return `${day} ${months[month]}, ${time} IST`;
+  return `${day} ${months[month]}, ${time.slice(0, 5)} IST`;
 };
