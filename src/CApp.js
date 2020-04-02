76709d88f24bcc120dad4baae335760cd98bb4ea
Bhuhsan Ivatury <bhushan@magically.in>
2020-04-02 19:21:00 +0530

    new for our purpose

--- src/CApp.js
+++ src/CApp.js
@@ -27,6 +27,30 @@ function App() {
       displayName: 'Home',
       animationDelayForNavbar: 0.2,
     },
+    /* {
+      pageLink: '/patientsDB',
+      view: PatientDB,
+      displayName: 'Patients DB',
+      animationDelayForNavbar: 0.3,
+    },*/
+    {
+      pageLink: '/clusters',
+      view: Cluster,
+      displayName: 'Clusters',
+      animationDelayForNavbar: 0.4,
+    },
+    {
+      pageLink: '/links',
+      view: Links,
+      displayName: 'Helpful Links',
+      animationDelayForNavbar: 0.5,
+    },
+    {
+      pageLink: '/faq',
+      view: FAQ,
+      displayName: 'FAQ',
+      animationDelayForNavbar: 0.6,
+    },
   ];
 
   return (
@@ -61,8 +85,42 @@ function App() {
           alt="https://www.covid19india.org | Coronavirus cases live dashboard"
         />*/}
 
-        <h5>Many thanks to the wonderful team at covid19india.org team</h5>
-        
+        <h5>We stand with everyone fighting on the frontlines</h5>
+        <div className="link">
+          <a href="https://github.com/covid19india">covid19india</a>
+        </div>
+        <a
+          href="https://github.com/covid19india/covid19india-react"
+          className="button github"
+        >
+          <Icon.GitHub />
+          <span>Open Sourced on GitHub</span>
+        </a>
+        <a
+          className="button excel"
+          href="https://bit.ly/patientdb"
+          target="_noblank"
+        >
+          <Icon.Database />
+          <span>Crowdsourced Patient Database&nbsp;</span>
+        </a>
+        <a
+          href="https://twitter.com/covid19indiaorg"
+          target="_noblank"
+          className="button twitter"
+          style={{justifyContent: 'center'}}
+        >
+          <Icon.Twitter />
+          <span>View updates on Twitter</span>
+        </a>
+        <a
+          href="https://bit.ly/covid19crowd"
+          className="button telegram"
+          target="_noblank"
+        >
+          <Icon.MessageCircle />
+          <span>Join Telegram to Collaborate!</span>
+        </a>
       </footer>
     </div>
   );
