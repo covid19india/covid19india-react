17425dde94358d11dff143db0f0e19a1a0841abc
triedcatched <46699194+triedcatched@users.noreply.github.com>
2020-04-04 07:35:55 +0000

    Deploying to gh-pages from  @ c2ca2c55c43e72bbb2509c03303bd962a11aeca6 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7fc126469188f3cee556b0f35521472.js"
+  "/precache-manifest.83d199a1aa5329e95f6d9bc13296b1f3.js"
 );
 
 self.addEventListener('message', (event) => {
