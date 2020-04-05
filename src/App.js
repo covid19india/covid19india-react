9570c45256d8d79d90eae561d222023a75acdb35
Himanshu Bhagwani <bhagwanihimanshu@gmail.com>
2020-04-05 17:34:49 +0530

    refactored code for footer component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import * as Icon from 'react-feather';
 
 import './App.scss';
 
@@ -14,6 +13,7 @@ import Navbar from './components/navbar';
 import Links from './components/links';
 import FAQ from './components/faq';
 import Banner from './components/banner';
+import Footer from './components/footer';
 /* import PatientDB from './components/patientdb';*/
 
 const history = require('history').createBrowserHistory;
@@ -67,54 +67,11 @@ function App() {
                   );
                 })}
               </Switch>
+              <Footer />
             </div>
           )}
         />
       </Router>
-
-      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-        {/* <img
-          src="/icon.png"
-          alt="https://www.covid19india.org | Coronavirus cases live dashboard"
-        />*/}
-
-        <h5>We stand with everyone fighting on the frontlines</h5>
-        <div className="link">
-          <a href="https://github.com/covid19india">covid19india</a>
-        </div>
-        <a
-          href="https://github.com/covid19india/covid19india-react"
-          className="button github"
-        >
-          <Icon.GitHub />
-          <span>Open Sourced on GitHub</span>
-        </a>
-        <a
-          className="button excel"
-          href="https://bit.ly/patientdb"
-          target="_noblank"
-        >
-          <Icon.Database />
-          <span>Crowdsourced Patient Database&nbsp;</span>
-        </a>
-        <a
-          href="https://twitter.com/covid19indiaorg"
-          target="_noblank"
-          className="button twitter"
-          style={{justifyContent: 'center'}}
-        >
-          <Icon.Twitter />
-          <span>View updates on Twitter</span>
-        </a>
-        <a
-          href="https://bit.ly/covid19crowd"
-          className="button telegram"
-          target="_noblank"
-        >
-          <Icon.MessageCircle />
-          <span>Join Telegram to Collaborate!</span>
-        </a>
-      </footer>
     </div>
   );
 }
