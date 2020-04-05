c21e2eeac9dc1f0938f0ab3f54bcde34e8023011
Chinmay Dalvi <chimay.dalvi@here.com>
2020-04-06 04:00:37 +0530

    added file to gitignore

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,4 @@ export const MAP_TYPES = {
 
 export const MAPS_DIR = '/maps';
 
-export const POLLING_FREQUENCY = 900000; // 15 minutes for auto refresh the page
+export const POLLING_FREQUENCY = 900000; // 15 minutes for auto update the page
