3d6c7ef121e18bb72e8287c9bf34303d1a49a469
Pravin <pravin@TM-ND0053.local>
2020-04-05 16:03:46 +0530

    Unnecessary URLs in 'Helpful Links' - 671

--- src/components/links.js
+++ src/components/links.js
@@ -4,51 +4,49 @@ function Links(props) {
   return (
     <div className="Links">
       <div className="link fadeInUp" style={{animationDelay: '0.2s'}}>
-        <h3>HELPLINE NUMBERS [by State]</h3>
         <a
           href="https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf
+          HELPLINE NUMBERS [by State]
         </a>
       </div>
 
       <div className="link fadeInUp" style={{animationDelay: '0.3s'}}>
-        <h3>Ministry of Health and Family Welfare, Gov. of India</h3>
+        
         <a
           href="https://www.mohfw.gov.in/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://www.mohfw.gov.in/
+          Ministry of Health and Family Welfare, Gov. of India
         </a>
       </div>
 
       <div className="link fadeInUp" style={{animationDelay: '0.4s'}}>
-        <h3>WHO : COVID-19 Home Page</h3>
+        
         <a
           href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://www.who.int/emergencies/diseases/novel-coronavirus-2019
+          WORLD HEALTH ORGANIZATION : COVID-19 Home Page
         </a>
       </div>
 
       <div className="link fadeInUp" style={{animationDelay: '0.5s'}}>
-        <h3>CDC</h3>
         <a
           href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://www.cdc.gov/coronavirus/2019-ncov/faq.html
+          CENTERS FOR DISEASE CONTROL AND PREVENTION
         </a>
       </div>
 
-      <div className="link fadeInUp" style={{animationDelay: '0.6s'}}>
-        <h3>Crowdsourced list of Resources & Essentials from across India</h3>
+      {/* <div className="link fadeInUp" style={{animationDelay: '0.6s'}}>
+        Crowdsourced list of Resources & Essentials from across India
         <a
           href="https://bit.ly/covid19resourcelist"
           target="_blank"
@@ -56,16 +54,16 @@ function Links(props) {
         >
           https://bit.ly/covid19resourcelist
         </a>
-      </div>
+      </div> */}
 
       <div className="link fadeInUp" style={{animationDelay: '0.7s'}}>
-        <h3>COVID-19 Global Tracker</h3>
+        
         <a
           href="https://coronavirus.thebaselab.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://coronavirus.thebaselab.com/
+          COVID-19 Global Tracker
         </a>
       </div>
     </div>
