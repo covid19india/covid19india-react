ce3c3a15829d56f7fc57db8cc1183b398bd83f0f
Ayush Shukla <shuklaayush247@gmail.com>
2020-04-06 21:52:45 +0530

    Create a new file for resizeObserver hook

---
+++ src/utils/hooks.js
@@ -0,0 +1,18 @@
+import ResizeObserver from 'resize-observer-polyfill';
+
+export const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect);
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
