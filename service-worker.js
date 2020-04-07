7b89aa61c9049970e104d49e55e049cc260507c8
JunaidBabu <junaidvayoli@gmail.com>
2020-04-07 14:33:12 +0000

    Deploying to gh-pages from  @ e3df2ab74aceab3451dc483eace17fa08f87aacb 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9912d78fb45217272ccfc049328156fc.js"
+  "/precache-manifest.844698320cc3d55e8b3246b3da48641f.js"
 );
 
 self.addEventListener('message', (event) => {
