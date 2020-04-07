cfc4ea8a940f1e8e6381b8e114202131c7be6dfd
huzefa <huzefa.biyawarwala@sell.do>
2020-04-07 09:42:12 +0530

    fix for mobile ratio alignment

--- src/components/minigraph.js
+++ src/components/minigraph.js
@@ -305,7 +305,7 @@ function Minigraph(props) {
       <div className="svg-parent fadeInUp" style={{animationDelay: '1.4s'}}>
         <svg
           ref={graphElement1}
-          width="100"
+          width="75"
           height="100"
           viewBox="0 0 100 100"
           preserveAspectRatio="xMidYMid meet"
@@ -318,7 +318,7 @@ function Minigraph(props) {
       >
         <svg
           ref={graphElement2}
-          width="100"
+          width="75"
           height="100"
           viewBox="0 0 100 100"
           preserveAspectRatio="xMidYMid meet"
@@ -331,7 +331,7 @@ function Minigraph(props) {
       >
         <svg
           ref={graphElement3}
-          width="100"
+          width="75"
           height="100"
           viewBox="0 0 100 100"
           preserveAspectRatio="xMidYMid meet"
@@ -344,7 +344,7 @@ function Minigraph(props) {
       >
         <svg
           ref={graphElement4}
-          width="100"
+          width="75"
           height="100"
           viewBox="0 0 100 100"
           preserveAspectRatio="xMidYMid meet"
@@ -356,7 +356,7 @@ function Minigraph(props) {
       >
         <svg
           ref={graphElement5}
-          width="100"
+          width="75"
           height="100"
           viewBox="0 0 100 100"
           preserveAspectRatio="xMidYMid meet"
