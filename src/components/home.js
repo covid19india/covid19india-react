ae35cd94bccd9e049bd41e7816f9a89e61dd56bb
jeremyphilemon <jeremyphilemon@outlook.com>
2020-04-07 04:36:52 +0300

    Added support for logs.
    Made design tweaks.

--- src/components/home.js
+++ src/components/home.js
@@ -6,15 +6,14 @@ import {
   formatDateAbsolute,
   validateCTS,
 } from '../utils/common-functions';
-/* import * as Icon from 'react-feather';
-import {Link} from 'react-router-dom';*/
+import * as Icon from 'react-feather';
+import {Link} from 'react-router-dom';
 
 import Table from './table';
 import Level from './level';
 import MapExplorer from './mapexplorer';
 import TimeSeries from './timeseries';
 import Minigraph from './minigraph';
-/* import Patients from './patients';*/
 
 function Home(props) {
   const [states, setStates] = useState([]);
@@ -68,150 +67,122 @@ function Home(props) {
   };
 
   return (
-    <div className="Home">
-      <div className="home-left">
-        <div className="header fadeInUp" style={{animationDelay: '1s'}}>
-          <div className="header-mid">
-            <div className="titles">
-              <h1>India COVID-19 Tracker</h1>
-              <h6 style={{fontWeight: 600}}>A Crowdsourced Initiative</h6>
-            </div>
-            <div className="last-update">
-              <h6>Last Updated</h6>
-              <h6 style={{color: '#28a745', fontWeight: 600}}>
-                {isNaN(Date.parse(formatDate(lastUpdated)))
-                  ? ''
-                  : formatDistance(
-                      new Date(formatDate(lastUpdated)),
-                      new Date()
-                    ) + ' Ago'}
-              </h6>
-              <h6 style={{color: '#28a745', fontWeight: 600}}>
-                {isNaN(Date.parse(formatDate(lastUpdated)))
-                  ? ''
-                  : formatDateAbsolute(lastUpdated)}
-              </h6>
-            </div>
-          </div>
-        </div>
-
-        {states.length > 1 && <Level data={states} />}
-        <Minigraph timeseries={timeseries} animate={true} />
-        <Table
-          states={states}
-          summary={false}
-          stateDistrictWiseData={stateDistrictWiseData}
-          onHighlightState={onHighlightState}
-          onHighlightDistrict={onHighlightDistrict}
-        />
-
-        <div
-          className="timeseries-header fadeInUp"
-          style={{animationDelay: '1.5s'}}
-        >
-          <h1>Activity Center</h1>
-          {timeseries && timeseries.length && (
-            <h4>{timeseries[timeseries.length - 1].date}</h4>
-          )}
-
-          {activityLog && activityLog.length && (
-            <div className="activity-center">
-              {activityLog
-                .slice()
-                .reverse()
-                .map(function (activity, index) {
-                  return (
-                    <p key={index} className="activity">
-                      {formatDistance(
-                        new Date(activity.timestamp * 1000),
+    <React.Fragment>
+      <div className="Home">
+        <div className="home-left">
+          <div className="header fadeInUp" style={{animationDelay: '1s'}}>
+            <div className="header-mid">
+              <div className="titles">
+                <h1>India COVID-19 Tracker</h1>
+                <h6 style={{fontWeight: 600}}>A Crowdsourced Initiative</h6>
+              </div>
+              <div className="last-update">
+                <h6>Last Updated</h6>
+                <h6 style={{color: '#28a745', fontWeight: 600}}>
+                  {isNaN(Date.parse(formatDate(lastUpdated)))
+                    ? ''
+                    : formatDistance(
+                        new Date(formatDate(lastUpdated)),
                         new Date()
                       ) + ' Ago'}
-                      : {activity.update}
-                    </p>
-                  );
-                })}
+                </h6>
+                <h6 style={{color: '#28a745', fontWeight: 600}}>
+                  {isNaN(Date.parse(formatDate(lastUpdated)))
+                    ? ''
+                    : formatDateAbsolute(lastUpdated)}
+                </h6>
+              </div>
             </div>
+          </div>
+
+          {states.length > 1 && <Level data={states} />}
+          <Minigraph timeseries={timeseries} animate={true} />
+          <Table
+            states={states}
+            summary={false}
+            stateDistrictWiseData={stateDistrictWiseData}
+            onHighlightState={onHighlightState}
+            onHighlightDistrict={onHighlightDistrict}
+          />
+        </div>
+
+        <div className="home-right">
+          {fetched && (
+            <React.Fragment>
+              <MapExplorer
+                states={states}
+                stateDistrictWiseData={stateDistrictWiseData}
+                regionHighlighted={regionHighlighted}
+              />
+
+              <div
+                className="timeseries-header fadeInUp"
+                style={{animationDelay: '2.5s'}}
+              >
+                <h1>Spread Trends</h1>
+                <div className="tabs">
+                  <div
+                    className={`tab ${graphOption === 1 ? 'focused' : ''}`}
+                    onClick={() => {
+                      setGraphOption(1);
+                    }}
+                  >
+                    <h4>Cumulative</h4>
+                  </div>
+                  <div
+                    className={`tab ${graphOption === 2 ? 'focused' : ''}`}
+                    onClick={() => {
+                      setGraphOption(2);
+                    }}
+                  >
+                    <h4>Daily</h4>
+                  </div>
+                </div>
+
+                <div className="scale-modes">
+                  <label>Scale Modes</label>
+                  <div className="timeseries-mode">
+                    <label htmlFor="timeseries-mode">Uniform</label>
+                    <input
+                      type="checkbox"
+                      checked={timeseriesMode}
+                      className="switch"
+                      aria-label="Checked by default to scale uniformly."
+                      onChange={(event) => {
+                        setTimeseriesMode(!timeseriesMode);
+                      }}
+                    />
+                  </div>
+                  <div
+                    className={`timeseries-logmode ${
+                      graphOption !== 1 ? 'disabled' : ''
+                    }`}
+                  >
+                    <label htmlFor="timeseries-logmode">Logarithmic</label>
+                    <input
+                      type="checkbox"
+                      checked={graphOption === 1 && timeseriesLogMode}
+                      className="switch"
+                      disabled={graphOption !== 1}
+                      onChange={(event) => {
+                        setTimeseriesLogMode(!timeseriesLogMode);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <TimeSeries
+                timeseries={timeseries}
+                type={graphOption}
+                mode={timeseriesMode}
+                logMode={timeseriesLogMode}
+              />
+            </React.Fragment>
           )}
         </div>
-      </div>
 
-      <div className="home-right">
-        {fetched && (
-          <React.Fragment>
-            <MapExplorer
-              states={states}
-              stateDistrictWiseData={stateDistrictWiseData}
-              regionHighlighted={regionHighlighted}
-            />
-
-            <div
-              className="timeseries-header fadeInUp"
-              style={{animationDelay: '2.3s'}}
-            >
-              <h1>Spread Trends</h1>
-              <div className="tabs">
-                <div
-                  className={`tab ${graphOption === 1 ? 'focused' : ''}`}
-                  onClick={() => {
-                    setGraphOption(1);
-                  }}
-                >
-                  <h4>Cumulative</h4>
-                </div>
-                <div
-                  className={`tab ${graphOption === 2 ? 'focused' : ''}`}
-                  onClick={() => {
-                    setGraphOption(2);
-                  }}
-                >
-                  <h4>Daily</h4>
-                </div>
-              </div>
-
-              <div className="scale-modes">
-                <label>Scale Modes</label>
-                <div className="timeseries-mode">
-                  <label htmlFor="timeseries-mode">Uniform</label>
-                  <input
-                    type="checkbox"
-                    checked={timeseriesMode}
-                    className="switch"
-                    aria-label="Checked by default to scale uniformly."
-                    onChange={(event) => {
-                      setTimeseriesMode(!timeseriesMode);
-                    }}
-                  />
-                </div>
-                <div
-                  className={`timeseries-logmode ${
-                    graphOption !== 1 ? 'disabled' : ''
-                  }`}
-                >
-                  <label htmlFor="timeseries-logmode">Logarithmic</label>
-                  <input
-                    type="checkbox"
-                    checked={graphOption === 1 && timeseriesLogMode}
-                    className="switch"
-                    disabled={graphOption !== 1}
-                    onChange={(event) => {
-                      setTimeseriesLogMode(!timeseriesLogMode);
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <TimeSeries
-              timeseries={timeseries}
-              type={graphOption}
-              mode={timeseriesMode}
-              logMode={timeseriesLogMode}
-            />
-          </React.Fragment>
-        )}
-      </div>
-
-      {/* <div className="home-left">
+        {/* <div className="home-left">
         {patients.length > 1 && (
           <div className="patients-summary">
             <h1>Recent Cases</h1>
@@ -245,7 +216,49 @@ function Home(props) {
       </div>
       <div className="home-right"></div>
     */}
-    </div>
+      </div>
+
+      <div className="Home">
+        <div className="home-left">
+          <div
+            className="updates-header fadeInUp"
+            style={{animationDelay: '1.5s'}}
+          >
+            <h1>Updates</h1>
+            {timeseries && timeseries.length && (
+              <h2>{timeseries[timeseries.length - 1].date}</h2>
+            )}
+          </div>
+
+          <div className="updates fadeInUp" style={{animationDelay: '1.7s'}}>
+            {activityLog
+              .slice(-5)
+              .reverse()
+              .map(function (activity, index) {
+                return (
+                  <div key={index} className="update">
+                    <h5>
+                      {formatDistance(
+                        new Date(activity.timestamp * 1000),
+                        new Date()
+                      ) + ' Ago'}
+                    </h5>
+                    <h4>{activity.update}</h4>
+                  </div>
+                );
+              })}
+            <button className="button">
+              <Link to="/database">
+                <Icon.Database />
+                <span>View the Patient Database</span>
+              </Link>
+            </button>
+          </div>
+        </div>
+
+        <div className="home-right"></div>
+      </div>
+    </React.Fragment>
   );
 }
 
