6eb614791d5d3e6876c287906601bdee2b7add6d
Kartikey Tanna <tannakartikey@gmail.com>
2020-04-11 16:22:31 +0530

    #355 Removed static specs for the map and the state data

--- src/__tests__/stateNames.test.js
+++ src/__tests__/stateNames.test.js
@@ -3,49 +3,23 @@ import {
   getStatesAndDistrictsFromMaps,
 } from '../testUtils/index';
 
-import staticStatesAndDistrictsFromAPI from '../testUtils/data/statesAndDistrictsFromAPI.json';
-import staticStatesAndDistrictsFromMaps from '../testUtils/data/statesAndDistrictsFromMaps.json';
+describe('Compare the map and the API data', () => {
+  test('for any discrepancies', async () => {
+    const statesAndDistrictsFromAPI = await getStatesAndDistrictsFromAPI();
+    const statesAndDistrictsFromMaps = await getStatesAndDistrictsFromMaps();
 
-describe('Compare the map and the API', () => {
-  describe('with dynamic data', () => {
-    test('for any discrepancies', async () => {
-      const statesAndDistrictsFromAPI = await getStatesAndDistrictsFromAPI();
-      const statesAndDistrictsFromMaps = await getStatesAndDistrictsFromMaps();
+    const statesFromAPI = Object.keys(statesAndDistrictsFromAPI);
+    const statesFromMaps = Object.keys(statesAndDistrictsFromMaps);
 
-      const statesFromAPI = Object.keys(statesAndDistrictsFromAPI);
-      const statesFromMaps = Object.keys(statesAndDistrictsFromMaps);
+    expect(statesFromAPI).toContain(...statesFromMaps);
 
-      expect(statesFromAPI).toContain(...statesFromMaps);
-
-      statesFromAPI.forEach((state) => {
-        const expectedDistricts = statesAndDistrictsFromMaps[state]?.sort();
-        const receivedDistricts = statesAndDistrictsFromAPI[state]?.sort();
-
-        if (expectedDistricts !== undefined)
-          expect(expectedDistricts).toContain(...receivedDistricts);
-      });
-    });
-  });
-
-  describe('from stored response for any discrepancies', () => {
-    const statesFromAPI = Object.keys(staticStatesAndDistrictsFromAPI);
-    const statesFromMaps = Object.keys(staticStatesAndDistrictsFromMaps);
     statesFromAPI.forEach((state) => {
-      test(`${state}`, async () => {
-        const expectedState = staticStatesAndDistrictsFromMaps[state]?.sort();
-        const receivedState = staticStatesAndDistrictsFromAPI[state]?.sort();
+      const expectedDistricts = statesAndDistrictsFromMaps[state]?.sort();
+      const receivedDistricts = statesAndDistrictsFromAPI[state]?.sort();
 
-        expect(
-          statesFromMaps
-            .map((e) => e.toLowerCase())
-            .indexOf(state.toLowerCase())
-        ).not.toEqual(-1);
-
-        const expected = expectedState.filter((e) => e !== 'Unknown').sort();
-        const received = receivedState.filter((e) => e !== 'Unknown').sort();
-
-        expect(expected).toContain(...received);
-      });
+      if (expectedDistricts !== undefined && receivedDistricts.length !== 0) {
+        expect(expectedDistricts).toContain(...receivedDistricts);
+      }
     });
   });
 });
