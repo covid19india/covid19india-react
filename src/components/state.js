298d7e322f15e0099e54d531ac36aab4f7470878
Ayush Shukla <shuklaayush247@gmail.com>
2020-04-14 02:04:58 +0530

    Add level and minigraphs to state page
    Add testing numbers

--- src/components/state.js
+++ src/components/state.js
@@ -1,30 +1,107 @@
-import React from 'react';
+import axios from 'axios';
+import {format, parse} from 'date-fns';
+import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect, useRef, useCallback} from 'react';
+import * as Icon from 'react-feather';
 import {Link} from 'react-router-dom';
 
+import {formatNumber, parseStateTimeseries} from '../utils/common-functions';
+import {STATE_CODES} from '../constants';
+
+import Level from './level';
+import Minigraph from './minigraph';
+
 function State(props) {
+  // const [data, setData] = useState(props.data);
+  const [fetched, setFetched] = useState(false);
+  const [timeseries, setTimeseries] = useState({});
+  const [stateData, setStateData] = useState({});
+  const [testData, setTestData] = useState({});
+  // const [stateDistrictWiseData, setStateDistrictWiseData] = useState({});
+  // const [stateTestData, setStateTestData] = useState({});
+  // const [timeseriesMode, setTimeseriesMode] = useState(true);
+  // const [timeseriesLogMode, setTimeseriesLogMode] = useState(false);
+
+  // Assuming routing is already handled, this would always correspond to a state code
+  const stateCode = window.location.pathname.split('/').pop().toUpperCase();
+  const stateName = STATE_CODES[stateCode];
+
+  useEffect(() => {
+    if (fetched === false) {
+      getState(stateCode);
+    }
+  }, [fetched, stateCode]);
+
+  const getState = async (code) => {
+    try {
+      const [
+        {data: dataResponse},
+        // stateDistrictWiseResponse,
+        {data: statesDailyResponse},
+        {data: stateTestResponse},
+      ] = await Promise.all([
+        axios.get('https://api.covid19india.org/data.json'),
+        // axios.get('https://api.covid19india.org/state_district_wise.json'),
+        axios.get('https://api.covid19india.org/states_daily.json'),
+        axios.get('https://api.covid19india.org/state_test_data.json'),
+      ]);
+      const states = dataResponse.statewise;
+      setStateData(states.find((s) => s.statecode === code));
+      const ts = parseStateTimeseries(statesDailyResponse)[code];
+      setTimeseries(ts);
+      // setLastUpdated(response.data.statewise[0].lastupdatedtime);
+      const statesTests = stateTestResponse.states_tested_data.reverse();
+      const name = STATE_CODES[code];
+      setTestData(
+        statesTests.find((obj) => obj.state === name && obj.totaltested !== '')
+      );
+      // setStateDistrictWiseData(stateDistrictWiseResponse.data);
+      setFetched(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="State">
-      <div className="state-left">
-        <div className="breadcrumb">
-          <Link to="/">Home</Link> / <Link to="state/Karnataka">Karnataka</Link>
-        </div>
-        <div className="header">
-          <div className="header-left">
-            <h1>Karnataka</h1>
-            <h5>11 Apr, 04:32 IST</h5>
+    <React.Fragment>
+      <div className="State">
+        <div className="state-left">
+          <div className="breadcrumb">
+            <Link to="/">Home</Link>
+            <Link to={`${stateCode}`}>{stateName}</Link>
           </div>
-          <div className="header-right">
-            <h5>Tested</h5>
-            <h2>30,000</h2>
-            <h5>As of 12 April</h5>
-            <h5>
-              per <a href="https://google.com">state bulletin</a>
-            </h5>
+          <div className="header">
+            <div className="header-left">
+              <h1>{stateName}</h1>
+              <h5>11 Apr, 04:32 IST</h5>
+            </div>
+            <div className="header-right">
+              <h5>Tested</h5>
+              <h2>{formatNumber(testData?.totaltested)}</h2>
+              <h5 className="timestamp">
+                {!isNaN(parse(testData?.updatedon, 'dd/MM/yyyy', new Date()))
+                  ? `As of ${format(
+                      parse(testData?.updatedon, 'dd/MM/yyyy', new Date()),
+                      'dd MMM'
+                    )}`
+                  : ''}
+              </h5>
+              <h5>
+                {testData?.totaltested && (
+                  <a href={testData.source} target="_noblank">
+                    <Icon.Link />
+                  </a>
+                )}
+              </h5>
+            </div>
           </div>
+
+          <Level data={stateData} />
+          {fetched && <Minigraph timeseries={timeseries} />}
         </div>
+        <div className="state-right">{/* map*/}</div>
       </div>
-      <div className="state-right"></div>
-    </div>
+    </React.Fragment>
   );
 }
 
