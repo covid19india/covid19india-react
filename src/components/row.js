a933e67c8a6490ce6259c680765faaa3ef2dcc6a
Ayush Shukla <shuklaayush247@gmail.com>
2020-04-14 13:31:40 +0530

    Disable touch events on table

--- src/components/row.js
+++ src/components/row.js
@@ -91,7 +91,7 @@ function Row(props) {
         className={props.total ? 'state is-total' : 'state'}
         onMouseEnter={() => props.onHighlightState?.(state, props.index)}
         onMouseLeave={() => props.onHighlightState?.()}
-        onTouchStart={() => props.onHighlightState?.(state, props.index)}
+        /* onTouchStart={() => props.onHighlightState?.(state, props.index)}*/
         onClick={!props.total ? handleReveal : null}
         style={{background: props.index % 2 === 0 ? '#f8f9fa' : ''}}
       >
@@ -255,9 +255,9 @@ function Row(props) {
                     props.onHighlightDistrict?.(district, state, props.index)
                   }
                   onMouseLeave={() => props.onHighlightDistrict?.()}
-                  onTouchStart={() =>
+                  /* onTouchStart={() =>
                     props.onHighlightDistrict?.(district, state, props.index)
-                  }
+                  }*/
                 >
                   <td style={{fontWeight: 600}}>{district}</td>
                   <td>
