186f8ca87e89c71a24440ff4ce09efcb58a1eaf9
Ayush Shukla <shuklaayush247@gmail.com>
2020-04-15 16:14:35 +0530

    Switch to force-graph for creating network

--- src/components/clusters.js
+++ src/components/clusters.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import * as d3 from 'd3';
-import React, {useEffect, useRef, useState} from 'react';
+import ForceGraph2D from 'react-force-graph-2d';
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 
 function Clusters({stateCode}) {
   const [fetched, setFetched] = useState(false);
   const [stateRawData, setStateRawData] = useState([]);
   const [networkData, setNetworkData] = useState([]);
-  const svgRef = useRef();
 
   function prepareNetworkData(data) {
     // Parse data
@@ -68,7 +68,8 @@ function Clusters({stateCode}) {
           'https://api.covid19india.org/raw_data.json'
         );
         setStateRawData(
-          rawDataResponse.data.raw_data.filter((d) => d.statecode === stateCode)
+          // rawDataResponse.data.raw_data.filter((d) => d.statecode === stateCode)
+          rawDataResponse.data.raw_data
         );
         setFetched(true);
       } catch (err) {
@@ -84,114 +85,15 @@ function Clusters({stateCode}) {
     setNetworkData(prepareNetworkData(stateRawData));
   }, [stateRawData]);
 
-  const drag = (simulation) => {
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    return d3
-      .drag()
-      .on('start', dragstarted)
-      .on('drag', dragged)
-      .on('end', dragended);
-  };
-
   useEffect(() => {
     if (!fetched) return;
-    const svg = d3.select(svgRef.current);
-    svg.selectAll('*').remove();
-
-    const width = +svg.attr('width');
-    const height = +svg.attr('height');
-    svg.attr('viewBox', [-width / 2, -height / 2, width, height]);
-
-    const colorScale = d3
-      .scaleOrdinal(d3.schemeCategory10)
-      .domain(['target', 'source']);
-    const radius = 3;
-
-    // Network graph
-    const nodes = networkData.nodes.map((d) => Object.create(d));
-    const links = networkData.links.map((d) => Object.create(d));
-
-    // Custom force to keep everything inside box
-    function boxForce() {
-      for (let i = 0, n = nodes.length; i < n; ++i) {
-        const currNode = nodes[i];
-        currNode.x = Math.max(
-          -width / 2 + radius,
-          Math.min(width / 2 - radius, currNode.x)
-        );
-        currNode.y = Math.max(
-          -height / 2 + radius,
-          Math.min(height / 2 - radius, currNode.y)
-        );
-      }
-    }
-
-    const simulation = d3
-      .forceSimulation(nodes)
-      .force(
-        'link',
-        d3.forceLink(links).id((d) => d.id)
-      )
-      .force('charge', d3.forceManyBody())
-      .force('collision', d3.forceCollide())
-      .force('x', d3.forceX().strength(0.2))
-      .force('y', d3.forceY().strength(0.2))
-      .force('boxForce', boxForce);
-
-    const link = svg
-      .append('g')
-      .attr('stroke', '#999')
-      .attr('stroke-opacity', 0.6)
-      .selectAll('line')
-      .data(links)
-      .join('line')
-      .attr('stroke-width', (d) => Math.sqrt(d.value));
-
-    const node = svg
-      .append('g')
-      .attr('stroke', '#fff')
-      .attr('stroke-width', 1.5)
-      .selectAll('circle')
-      .data(nodes)
-      .join('circle')
-      .attr('r', radius)
-      .attr('fill', (d) => colorScale(d.group))
-      .call(drag(simulation));
-
-    node.append('title').text((d) => JSON.stringify(d.raw, null, 2));
-
-    simulation.on('tick', () => {
-      link
-        .attr('x1', (d) => d.source.x)
-        .attr('y1', (d) => d.source.y)
-        .attr('x2', (d) => d.target.x)
-        .attr('y2', (d) => d.target.y);
-
-      node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
-    });
+    ReactDOM.render(
+      <ForceGraph2D graphData={networkData} nodeLabel="id" />,
+      document.getElementById('clusters')
+    );
   }, [fetched, networkData]);
 
-  return (
-    <div className="Clusters">
-      <svg id="clusters" width="500" height="500" ref={svgRef}></svg>
-    </div>
-  );
+  return <div id="clusters" className="Clusters"></div>;
 }
 
 export default Clusters;
