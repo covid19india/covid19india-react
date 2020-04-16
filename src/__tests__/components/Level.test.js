03cc308ef4ee6f564e6573cd510fbb4ca6db66c0
Kartikey Tanna <tannakartikey@gmail.com>
2020-04-16 19:16:57 +0530

    Fixed Level spec to match updates from 00584fa82e87472952e0a3118af9811281d1e1ae

--- src/__tests__/components/Level.test.js
+++ src/__tests__/components/Level.test.js
@@ -2,32 +2,20 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import Level from '../../components/level';
 
-const states = [
-  {
-    deltaconfirmed: '10',
-    deltadeaths: '3',
-    deltarecovered: '5',
-  },
-  {
-    active: '75',
-    confirmed: '83',
-    deaths: '3',
-    recovered: '5',
-    state: 'Karnataka',
-  },
-  {
-    active: '5',
-    confirmed: '3',
-    deaths: '1',
-    recovered: '2',
-    state: 'Gujarat',
-  },
-];
+const state = {
+  active: '75',
+  confirmed: '83',
+  recovered: '5',
+  deaths: '3',
+  deltaconfirmed: '10',
+  deltadeaths: '3',
+  deltarecovered: '5',
+};
 
 test('Level renders total state data', () => {
-  const {container} = render(<Level data={states} />);
+  const {container} = render(<Level data={state} />);
 
   expect(container).toHaveTextContent(
-    'Confirmed[+10]86 Active 80Recovered[+5]7 Deceased[+3]4'
+    'Confirmed[+10]83 Active 75Recovered[+5]5 Deceased[+3]3'
   );
 });
