3681781b93dac6d0f2684cb27bc7004a996e09d4
Tarun Goel <tarun@grofers.com>
2020-04-17 23:37:39 +0530

    Add Null Check to handle app crashes in state

--- src/components/state.js
+++ src/components/state.js
@@ -159,18 +159,20 @@ function State(props) {
                 <Icon.Compass />
                 <div className="sources-right">
                   Data collected from sources{' '}
-                  {Object.keys(sources[0]).map((key) => {
-                    if (key.match('source') && sources[0][key] !== '') {
-                      const num = key.match(/\d+/);
-                      return (
-                        <React.Fragment>
-                          {num > 1 ? ',' : ''}
-                          <a href={sources[0][key]}>{num}</a>
-                        </React.Fragment>
-                      );
-                    }
-                    return null;
-                  })}
+                  {sources.length > 0
+                    ? Object.keys(sources[0]).map((key) => {
+                        if (key.match('source') && sources[0][key] !== '') {
+                          const num = key.match(/\d+/);
+                          return (
+                            <React.Fragment>
+                              {num > 1 ? ',' : ''}
+                              <a href={sources[0][key]}>{num}</a>
+                            </React.Fragment>
+                          );
+                        }
+                        return null;
+                      })
+                    : ''}
                 </div>
               </div>
             </div>
@@ -187,35 +189,40 @@ function State(props) {
                 >
                   <h2>Top districts</h2>
                   <div className="districts">
-                    {Object.keys(districtData[stateName].districtData)
-                      .slice(0, 6)
-                      .sort(
-                        (a, b) =>
-                          districtData[stateName].districtData[b].confirmed -
-                          districtData[stateName].districtData[a].confirmed
-                      )
-                      .map((district, index) => {
-                        return (
-                          <div key={index} className="district">
-                            <h2>
-                              {
-                                districtData[stateName].districtData[district]
-                                  .confirmed
-                              }
-                            </h2>
-                            <h5>{district}</h5>
-                            <div className="delta">
-                              <Icon.ArrowUp />
-                              <h6>
-                                {
-                                  districtData[stateName].districtData[district]
-                                    .delta.confirmed
-                                }
-                              </h6>
-                            </div>
-                          </div>
-                        );
-                      })}
+                    {districtData[stateName]
+                      ? Object.keys(districtData[stateName].districtData)
+                          .slice(0, 6)
+                          .sort(
+                            (a, b) =>
+                              districtData[stateName].districtData[b]
+                                .confirmed -
+                              districtData[stateName].districtData[a].confirmed
+                          )
+                          .map((district, index) => {
+                            return (
+                              <div key={index} className="district">
+                                <h2>
+                                  {
+                                    districtData[stateName].districtData[
+                                      district
+                                    ].confirmed
+                                  }
+                                </h2>
+                                <h5>{district}</h5>
+                                <div className="delta">
+                                  <Icon.ArrowUp />
+                                  <h6>
+                                    {
+                                      districtData[stateName].districtData[
+                                        district
+                                      ].delta.confirmed
+                                    }
+                                  </h6>
+                                </div>
+                              </div>
+                            );
+                          })
+                      : ''}
                   </div>
                 </div>
                 <div className="district-bar-right">
