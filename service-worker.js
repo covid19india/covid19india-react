0edb65c8449872729e517ee722d8c8347fa8bd51
jeremyphilemon <jeremyphilemon@outlook.com>
2020-04-26 16:06:19 +0000

    Deploying to gh-pages from  @ 131e8cea91a3b9fe809da9862b3f5bb93ac34531 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a2e3caa4ff5abef532f627bcae43ef7.js"
+  "/precache-manifest.a400259146181d950bfb3185996cddf4.js"
 );
 
 self.addEventListener('message', (event) => {
