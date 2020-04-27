9ed9f9b8d3c7486a2c70ff6277b556d2f03e4f22
Irshad Ali <irshadali786143@gmail.com>
2020-04-27 23:29:23 +0530

    Create worker.js

---
+++ public/worker.js
@@ -0,0 +1,48 @@
+Let CACHE_NAME = 'Covid19india;
+Let urlsToCache = [
+  '/',
+  '/completed'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  Let cacheWhitelist = ['your-app-name'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
