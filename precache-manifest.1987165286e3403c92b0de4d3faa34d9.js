6162a912f61fe8dbff94b38949e419f9fda1701c
jeremyphilemon <jeremyphilemon@outlook.com>
2020-04-27 16:48:56 +0000

    Deploying to gh-pages from  @ 17502f84a8c016b80767164bce57a99a537526b0 🚀

---
+++ precache-manifest.1987165286e3403c92b0de4d3faa34d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d139aff7b6194113651e7f336c49beb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f493da68fe6f5161618",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "7769d5ba979447272c28",
+    "url": "/static/css/main.8b68e7c9.chunk.css"
+  },
+  {
+    "revision": "5f493da68fe6f5161618",
+    "url": "/static/js/2.d23186c1.chunk.js"
+  },
+  {
+    "revision": "577b7a3b9fbfa17fccf2f4ddb5a84180",
+    "url": "/static/js/2.d23186c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7769d5ba979447272c28",
+    "url": "/static/js/main.856cddec.chunk.js"
+  },
+  {
+    "revision": "20e74bb56b5aad9a2c37",
+    "url": "/static/js/runtime-main.96a74c67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bfa2c9f6d3f001f5c67fc492a581f50.js"
+  "/precache-manifest.1987165286e3403c92b0de4d3faa34d9.js"
 );
 
 self.addEventListener('message', (event) => {
