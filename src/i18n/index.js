d0bdd597763841d8789c3a9665380858e2f31a98
Kartikey Tanna <tannakartikey@gmail.com>
2020-04-28 08:25:14 +0530

    Disabled i18n debug for the production env

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,12 +3,18 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import {initReactI18next} from 'react-i18next';
 
+const shouldDebug = () => {
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
+    return true;
+  return false;
+};
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: true,
+    debug: shouldDebug(),
     lng: 'en',
     fallbackLng: 'en',
     backend: {
