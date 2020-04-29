4151858c3b62463b8daa59d708fd19f7ad80a077
Binu42 <kbinu42@gmail.com>
2020-04-30 01:12:10 +0530

    added time duration

--- src/components/Charts/dailyconfirmedchart.js
+++ src/components/Charts/dailyconfirmedchart.js
@@ -11,24 +11,28 @@ import React from 'react';
 import {Bar, defaults} from 'react-chartjs-2';
 
 function DailyConfirmedChart(props) {
-  const dates = [];
-  const confirmed = [];
-  const recovered = [];
-  const deceased = [];
+  const [range, setRange] = React.useState(31);
+  let dates = [];
+  let confirmed = [];
+  let recovered = [];
+  let deceased = [];
 
   if (!props.timeseries || props.timeseries.length === 0) {
     return <div></div>;
   }
 
-  props.timeseries.forEach((data, index) => {
-    if (index >= 31) {
-      const date = parse(data.date, 'dd MMMM', new Date(2020, 0, 1));
-      dates.push(format(date, 'dd MMM'));
-      confirmed.push(data.dailyconfirmed);
-      recovered.push(data.dailyrecovered);
-      deceased.push(data.dailydeceased);
-    }
-  });
+  for (
+    let i = range !== 31 ? props.timeseries.length - range : range;
+    i < props.timeseries.length;
+    i++
+  ) {
+    const data = props.timeseries[i];
+    const date = parse(data.date, 'dd MMMM', new Date(2020, 0, 1));
+    dates.push(format(date, 'dd MMM'));
+    confirmed.push(data.dailyconfirmed);
+    recovered.push(data.dailyrecovered);
+    deceased.push(data.dailydeceased);
+  }
 
   const barDataSet = {
     labels: dates,
@@ -92,6 +96,26 @@ function DailyConfirmedChart(props) {
       <div className="chart-content">
         <Bar data={barDataSet} options={options} />
       </div>
+      <form
+        onChange={(e) => {
+          confirmed = [];
+          recovered = [];
+          deceased = [];
+          dates = [];
+          setRange(e.target.value);
+          console.log(range);
+        }}
+        style={{display: 'inline'}}
+      >
+        <input type="radio" id="begin" name="range" value={31} defaultChecked />
+        <label htmlFor="begin">Beginning</label>
+        <input type="radio" id="6weeks" name="range" value={56} />
+        <label htmlFor="6weeks">8 weeks</label>
+        <input type="radio" id="6weeks" name="range" value={42} />
+        <label htmlFor="6weeks">6 weeks</label>
+        <input type="radio" id="4weeks" name="range" value={28} />
+        <label htmlFor="4weeks">4 weeks</label>
+      </form>
     </div>
   );
 }
