4d59802ea608abbbb30edbbe9b9faae0539d69de
Kartikey Tanna <tannakartikey@gmail.com>
2020-05-05 08:45:57 +0530

    Renamed missing_locales.json to all_locales.json

--- utility/missingLocaleServer.js
+++ utility/missingLocaleServer.js
@@ -12,9 +12,9 @@ const server = http.createServer(async (req, res) => {
     })
     .on('end', () => {
       body = Buffer.concat(body).toString();
-      fs.openSync('missing_locales.json', 'a');
+      fs.openSync('all_locales.json', 'a');
       const enJson = fs.readFileSync('public/locales/en.json', 'utf-8');
-      const data = fs.readFileSync('missing_locales.json', 'utf-8');
+      const data = fs.readFileSync('all_locales.json', 'utf-8');
       console.log(`body is: ${body}`);
 
       const dataToWrite = JSON.stringify(
@@ -27,7 +27,7 @@ const server = http.createServer(async (req, res) => {
         2
       );
 
-      fs.writeFileSync('missing_locales.json', dataToWrite);
+      fs.writeFileSync('all_locales.json', dataToWrite);
 
       console.log(`${body} added to the file!`);
       res.writeHead(200, {
