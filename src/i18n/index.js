776556799486cce500699b625fdecba0237e81b0
Kumar Gulshan <gulshan@pawsnme.com>
2020-05-06 02:18:34 +0530

    changes for localization issue

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,22 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import XHR from 'i18next-xhr-backend';
+import {initReactI18next} from 'react-i18next';
 
-i18n.use(XHR).init({
-  debug: false,
-  lng: 'en',
-  fallbackLng: 'en',
-  backend: {
-    loadPath: '/locales/{{lng}}.json',
-  },
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: false,
+    lng: 'en',
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
