95edbb8a2a01be618084ebc7885cb8278015841a
shuklaayush <shuklaayush247@gmail.com>
2020-05-06 18:37:30 +0000

    Deploying to gh-pages from  @ e963433f12c6f39af573db3a382400c746a7046d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0709320419dbb81a992411af351c913f.js"
+  "/precache-manifest.a0325ee19daff44b6de6a7f7257295b7.js"
 );
 
 self.addEventListener('message', (event) => {
