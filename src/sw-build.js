c63699a40bb492c7c9d5c9e0ca5def1e776b5011
Kousik Satish <kousiksatish@gmail.com>
2020-05-06 23:55:12 +0530

    Feat: Support 'cache then network' service worker. (#1829)
    * Migrate to latest v5 of workox
    * Define basic caching strategies for various file types
    * Remove unused sw.js file
    * Add js and css to precache

---
+++ src/sw-build.js
@@ -0,0 +1,18 @@
+const workboxBuild = require('workbox-build');
+
+const buildSW = () => {
+  workboxBuild
+    .injectManifest({
+      swSrc: 'src/sw-template.js',
+      swDest: 'build/service-worker.js',
+      globDirectory: 'build',
+      globPatterns: ['**/*.{html,woff2,js,css}'], // precaching restricted to only html and fonts.
+    })
+    .then(({count, size, warnings}) => {
+      // Optionally, log any warnings and details.
+      warnings.forEach(console.warn);
+      console.log(`${count} files will be precached, totaling ${size} bytes.`);
+    })
+    .catch(console.error);
+};
+buildSW();
