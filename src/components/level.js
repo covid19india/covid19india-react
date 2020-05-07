ad015e7533c7db6d65c8e21707794f3da3a98343
ripudaman <ripudaman.kaushik@yahoo.in>
2020-05-08 00:41:57 +0530

    remove clg

--- src/components/level.js
+++ src/components/level.js
@@ -5,7 +5,6 @@ import {useTranslation} from 'react-i18next';
 import {useEffectOnce} from 'react-use';
 
 function getFontSize(number) {
-  console.log(number);
   if (number.length <= 4) {
     return 24;
   }
