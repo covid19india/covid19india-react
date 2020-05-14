1197668167cdad9a9933ba4dc6f3e8a71104eb1b
jeremyphilemon <jeremyphilemon@outlook.com>
2020-05-14 20:55:49 +0300

    Feat: Add content loader for essentials. (#1872)

--- src/components/essentials.js
+++ src/components/essentials.js
@@ -1,8 +1,52 @@
 import KnnResults from './knnresults';
 
 import axios from 'axios';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import ContentLoader from 'react-content-loader';
 import * as Icon from 'react-feather';
+import {useMeasure} from 'react-use';
+
+function EssentialsLoader() {
+  const [svgElement, {width}] = useMeasure();
+
+  useEffect(() => {}, [width]);
+
+  return (
+    <React.Fragment>
+      <span ref={svgElement} style={{width: '100%'}} />
+      {width && (
+        <ContentLoader
+          speed={1.5}
+          width={width}
+          height={325}
+          viewBox={`0 0 ${width} 325`}
+          position="absolute"
+          className="fadeInUp"
+        >
+          <rect
+            x={width / 2 - 60}
+            y="10"
+            rx="5"
+            ry="5"
+            width="120"
+            height="32"
+          />
+          <rect
+            x={width / 2 + 70}
+            y="18"
+            rx="100"
+            ry="100"
+            width="15"
+            height="15"
+          />
+          <rect x="10" y="80" rx="5" ry="5" width="85" height="32" />
+          <rect x="100" y="80" rx="5" ry="5" width="65" height="32" />
+          <rect x="10" y="130" rx="5" ry="5" width={width - 20} height="172" />
+        </ContentLoader>
+      )}
+    </React.Fragment>
+  );
+}
 
 const Essentials = (props) => {
   const [currentLocation, setCurrentLocation] = useState(null);
@@ -65,7 +109,7 @@ const Essentials = (props) => {
             <Icon.AlertOctagon size={16} />
             <div className="alert-right is-full">
               {`We do not collect any location data; they're all stored 
-              inside your browser and are inaccessible to no one except 
+              inside your browser and are inaccessible to us. 
               you.`}
             </div>
           </div>
@@ -81,6 +125,8 @@ const Essentials = (props) => {
           </div>
         </React.Fragment>
       )}
+
+      {currentLocation && !currentAddress && <EssentialsLoader />}
 
       {currentAddress && (
         <div className="address fadeInUp">
