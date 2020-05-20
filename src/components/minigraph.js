f787caa2c6e1845358f050e233d1f62de7780c8a
jeremyphilemon <jeremyphilemon@outlook.com>
2020-05-20 19:37:01 +0300

    Refactor: Abstract Minigraphs.

--- src/components/minigraph.js
+++ src/components/minigraph.js
@@ -1,15 +1,13 @@
 import {PRIMARY_STATISTICS} from '../constants';
 
+import classnames from 'classnames';
 import * as d3 from 'd3';
 import equal from 'fast-deep-equal';
 import produce from 'immer';
 import React, {useEffect, useRef, useCallback, useMemo} from 'react';
 
 function Minigraph({timeseries}) {
-  const svgRef1 = useRef();
-  const svgRef2 = useRef();
-  const svgRef3 = useRef();
-  const svgRef4 = useRef();
+  const refs = useRef([]);
 
   const dates = useMemo(() => {
     return Object.keys(timeseries).slice(-20);
@@ -29,17 +27,17 @@ function Minigraph({timeseries}) {
     const chartRight = 100 - margin.right;
     const chartBottom = 100 - margin.bottom;
 
-    const svg1 = d3.select(svgRef1.current);
-    const svg2 = d3.select(svgRef2.current);
-    const svg3 = d3.select(svgRef3.current);
-    const svg4 = d3.select(svgRef4.current);
+    const svgArray = [];
+    refs.current.forEach((ref) => {
+      svgArray.push(d3.select(ref));
+    });
+
+    console.log(svgArray);
 
     const xScale = d3
       .scaleTime()
       .domain([new Date(d3.min(dates)), new Date(d3.max(dates))])
       .range([margin.left, chartRight]);
-
-    const svgArray = [svg1, svg2, svg3, svg4];
     const colors = ['#ff073a', '#007bff', '#28a745', '#6c757d'];
 
     const dailyMin = d3.min(dates, (date) => timeseries[date].active);
@@ -50,6 +48,7 @@ function Minigraph({timeseries}) {
         timeseries[date].deceased
       )
     );
+
     const domainMinMax = Math.max(-dailyMin, dailyMax);
 
     const yScale = d3
@@ -58,7 +57,7 @@ function Minigraph({timeseries}) {
       .range([chartBottom, margin.top]);
 
     svgArray.forEach((svg, index) => {
-      const type = PRIMARY_STATISTICS[index];
+      const statistic = PRIMARY_STATISTICS[index];
       const color = colors[index];
 
       const path = svg
@@ -73,7 +72,7 @@ function Minigraph({timeseries}) {
           d3
             .line()
             .x((date) => xScale(new Date(date)))
-            .y((date) => yScale(timeseries[date][type]))
+            .y((date) => yScale(timeseries[date][statistic]))
             .curve(d3.curveMonotoneX)
         );
 
@@ -97,7 +96,7 @@ function Minigraph({timeseries}) {
         .attr('r', 2)
         .attr('cursor', 'pointer')
         .attr('cx', (date) => xScale(new Date(date)))
-        .attr('cy', (date) => yScale(timeseries[date][type]))
+        .attr('cy', (date) => yScale(timeseries[date][statistic]))
         .on('mouseover', (d) => {
           d3.select(d3.event.target).attr('r', '5');
         })
@@ -120,54 +119,19 @@ function Minigraph({timeseries}) {
 
   return (
     <div className="Minigraph">
-      <div className="svg-parent fadeInUp" style={{animationDelay: '1.4s'}}>
-        <svg
-          ref={svgRef1}
-          width="100"
-          height="75"
-          viewBox="0 0 100 75"
-          preserveAspectRatio="xMidYMid meet"
-        />
-      </div>
-
-      <div
-        className="svg-parent is-blue fadeInUp"
-        style={{animationDelay: '1.5s'}}
-      >
-        <svg
-          ref={svgRef2}
-          width="100"
-          height="75"
-          viewBox="0 0 100 75"
-          preserveAspectRatio="xMidYMid meet"
-        />
-      </div>
-
-      <div
-        className="svg-parent is-green fadeInUp"
-        style={{animationDelay: '1.6s'}}
-      >
-        <svg
-          ref={svgRef3}
-          width="100"
-          height="75"
-          viewBox="0 0 100 75"
-          preserveAspectRatio="xMidYMid meet"
-        />
-      </div>
-
-      <div
-        className="svg-parent is-gray fadeInUp"
-        style={{animationDelay: '1.7s'}}
-      >
-        <svg
-          ref={svgRef4}
-          width="100"
-          height="75"
-          viewBox="0 0 100 75"
-          preserveAspectRatio="xMidYMid meet"
-        />
-      </div>
+      {PRIMARY_STATISTICS.map((statistic) => (
+        <div key={statistic} className={classnames('svg-parent')}>
+          <svg
+            ref={(ref) => {
+              refs.current.push(ref);
+            }}
+            width="100"
+            height="75"
+            viewBox="0 0 100 75"
+            preserveAspectRatio="xMidYMid meet"
+          />
+        </div>
+      ))}
     </div>
   );
 }
