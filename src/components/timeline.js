ee9aa979dd5662dd61865aaf5bd2c381ee7253b6
jeremyphilemon <jeremyphilemon@outlook.com>
2020-05-28 18:01:11 +0300

    Feat: Add support for keypress events.

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,8 +1,8 @@
 import {addDays, formatISO, differenceInDays, format} from 'date-fns';
 import clamp from 'lodash/clamp';
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {useSprings, animated, config} from 'react-spring';
-import {useMeasure} from 'react-use';
+import {useMeasure, useKeyPressEvent} from 'react-use';
 import {useGesture} from 'react-use-gesture';
 
 const Timeline = ({setIsTimelineMode, setDate}) => {
@@ -17,7 +17,7 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
       color: i == 0 ? '#6c757d' : '#6c757d99',
       opacity: i < 2 ? 1 : 0,
     }),
-    config.gentle
+    config.stiff
   );
 
   const bind = useGesture(
@@ -30,8 +30,9 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
         cancel,
         elapsedTime,
         direction,
+        velocity,
       }) => {
-        if (down && distance > 75) {
+        if (down && distance > 30) {
           cancel(
             setIndex(clamp(index + (xDir > 0 ? 1 : -1), 0, days.length - 1))
           );
@@ -59,6 +60,42 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
           return {x, display: 'block', color: '#6c757d99', opacity: 1};
         });
       },
+      onWheel: ({
+        down,
+        delta: [xDelta],
+        direction: [xDir],
+        distance,
+        cancel,
+        offset: [xOffset],
+        direction,
+      }) => {
+        if (distance > 0) {
+          cancel(
+            setIndex(clamp(index + (xDir > 0 ? 1 : -1), 0, days.length - 1))
+          );
+
+          setDate(days[index]);
+        }
+
+        if (index === 0 && direction[0] === -1) {
+          setTimeout(() => {
+            setIsTimelineMode(false);
+          }, 1000);
+        }
+
+        set((i) => {
+          if (i < index - 1) {
+            return {x: width, color: '#6c757d99', opacity: 0};
+          } else if (i > index + 1) {
+            return {x: -40, color: '#6c757d99', opacity: 0};
+          }
+          const x = (index - i) * (width / 3) + width / 3 + 35;
+          if (i === index) {
+            return {x, display: 'block', color: '#6c757d'};
+          }
+          return {x, display: 'block', color: '#6c757d99', opacity: 1};
+        });
+      },
     },
     {rubberband: true}
   );
@@ -78,16 +115,45 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
     setDays(daysList.reverse());
   }, [days.length]);
 
+  const setSprings = (nextIndex) => {
+    if (nextIndex >= 0 && nextIndex < days.length - 2) {
+      set((i) => {
+        if (i < nextIndex - 1) {
+          return {x: width, color: '#6c757d99', opacity: 0};
+        } else if (i > nextIndex + 1) {
+          return {x: -40, color: '#6c757d99', opacity: 0};
+        }
+        const x = (nextIndex - i) * (width / 3) + width / 3 + 35;
+        if (i === nextIndex) {
+          return {x, display: 'block', color: '#6c757d'};
+        }
+        return {x, display: 'block', color: '#6c757d99', opacity: 1};
+      });
+
+      setIndex(nextIndex);
+      setDate(days[nextIndex + 1]);
+    }
+  };
+
+  useKeyPressEvent('ArrowLeft', () => {
+    setSprings(index + 1);
+  });
+
+  useKeyPressEvent('ArrowRight', () => {
+    setSprings(index - 1);
+  });
+
   return (
     <div className="Timeline" ref={timelineElement} {...bind()}>
       {springs
         .filter(
           ({opacity}, i) =>
-            i === index + 1 ||
-            i === index - 1 ||
-            i === index + 2 ||
-            i === index - 2 ||
-            i === index
+            i < days.length - 2 &&
+            (i === index + 2 ||
+              i === index - 2 ||
+              i === index + 1 ||
+              i === index - 1 ||
+              i === index)
         )
         .map(({x, color, opacity}, i) => (
           <animated.div
@@ -98,6 +164,7 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
               opacity,
             }}
           >
+            {console.log(index)}
             {index === 0 && (
               <React.Fragment>
                 <animated.h5 style={{color}}>
@@ -105,10 +172,17 @@ const Timeline = ({setIsTimelineMode, setDate}) => {
                 </animated.h5>
               </React.Fragment>
             )}
-            {index !== 0 && (
+            {index > 0 && index < days.length - 2 && (
               <React.Fragment>
                 <animated.h5 style={{color}}>
                   {format(new Date(days[index + i]), 'dd MMM')}
+                </animated.h5>
+              </React.Fragment>
+            )}
+            {index === days.length - 2 && (
+              <React.Fragment>
+                <animated.h5 style={{color}}>
+                  {format(new Date(days[index]), 'dd MMM')}
                 </animated.h5>
               </React.Fragment>
             )}
