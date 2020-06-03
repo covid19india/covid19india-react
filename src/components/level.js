ee65eea67e0a9f6112a0a96e4c2965fcd3ff6ab2
Ayush Shukla <shuklaayush247@gmail.com>
2020-06-03 14:35:15 +0530

    Refactor: Change SVG heart icon to unicode character to support transitions

--- src/components/level.js
+++ src/components/level.js
@@ -1,7 +1,6 @@
 import {PRIMARY_STATISTICS} from '../constants';
 import {capitalize, formatNumber, getStatistic} from '../utils/commonfunctions';
 
-import {HeartFillIcon} from '@primer/octicons-v2-react';
 import classnames from 'classnames';
 import equal from 'fast-deep-equal';
 import React from 'react';
@@ -24,12 +23,8 @@ function PureLevelItem({statistic, total, delta}) {
       <h5>{capitalize(t(statistic))}</h5>
       <h4>
         <animated.span>
-          {delta > 0 ? (
-            spring.delta.interpolate(
-              (delta) => `+${formatNumber(Math.floor(delta))}`
-            )
-          ) : (
-            <HeartFillIcon size={9} verticalAlign={2} />
+          {spring.delta.interpolate((delta) =>
+            delta > 0 ? `+${formatNumber(Math.floor(delta))}` : '\u2764'
           )}
         </animated.span>
       </h4>
