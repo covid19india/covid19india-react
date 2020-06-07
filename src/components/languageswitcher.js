21160dc2efc11238570281cac0472eef52006099
jeremyphilemon <jeremyphilemon@outlook.com>
2020-06-08 02:01:39 +0300

    Refactor: Use native LanguageSwitcher and StateDropdown.

--- src/components/languageswitcher.js
+++ src/components/languageswitcher.js
@@ -1,40 +1,75 @@
 import locales from '../i18n/locales.json';
 
-import Button from '@primer/components/lib/Button';
-import SelectMenu from '@primer/components/lib/SelectMenu';
+import classnames from 'classnames';
 import React from 'react';
+import {XCircle} from 'react-feather';
 import {useTranslation} from 'react-i18next';
+import {useTransition, animated} from 'react-spring';
 
-function LanguageSwitcher() {
+const FADE_IN = {
+  opacity: 1,
+  height: '12.5rem',
+  paddingTop: '7.5rem',
+  paddingBottom: '7.5rem',
+};
+
+const FADE_OUT = {
+  opacity: 0,
+  height: '0rem',
+  paddingTop: '0rem',
+  paddingBottom: '0rem',
+};
+
+function LanguageSwitcher({showLanguageSwitcher, setShowLanguageSwitcher}) {
   const {i18n} = useTranslation();
   const currentLanguage = Object.keys(locales).includes(i18n?.language)
     ? i18n?.language
     : i18n?.options?.fallbackLng[0];
 
-  return (
-    <div className="LanguageSwitcher">
-      <SelectMenu>
-        <Button as="summary" className="button">
-          {locales[currentLanguage]}
-        </Button>
-        <SelectMenu.Modal className="select-menu-modal">
-          <SelectMenu.List className="select-menu-list">
-            {Object.entries(locales).map(([key, language]) => (
-              <SelectMenu.Item
-                key={key}
-                className="select-menu-item"
-                onClick={() => {
-                  if (i18n) i18n.changeLanguage(key);
-                }}
-                selected={currentLanguage === key}
-              >
-                {language}
-              </SelectMenu.Item>
-            ))}
-          </SelectMenu.List>
-        </SelectMenu.Modal>
-      </SelectMenu>
-    </div>
+  const transitions = useTransition(showLanguageSwitcher, null, {
+    from: FADE_OUT,
+    enter: FADE_IN,
+    leave: FADE_OUT,
+    config: {
+      mass: 1,
+      tension: 210,
+      friction: 25,
+    },
+  });
+
+  return transitions.map(({item, key, props}) =>
+    item ? (
+      <animated.div key={key} className="LanguageSwitcher" style={props}>
+        <span
+          className="close-button"
+          onClick={() => {
+            setShowLanguageSwitcher(false);
+          }}
+        >
+          <XCircle width={16} />
+        </span>
+
+        <h3>We speak the following languages</h3>
+
+        <div className="languages">
+          {Object.keys(locales).map((languageKey) => (
+            <div
+              key={languageKey}
+              className={classnames('language', {
+                'is-highlighted': currentLanguage === languageKey,
+              })}
+              onClick={() => {
+                if (i18n) i18n.changeLanguage(languageKey);
+              }}
+            >
+              <span>{locales[languageKey]}</span>
+            </div>
+          ))}
+        </div>
+      </animated.div>
+    ) : (
+      <animated.div></animated.div>
+    )
   );
 }
 
